5f2ff6e7f3897083a047d92873ec0e0260f58b01
Gonzalo D'Elia <gonzalo@bloq.com>
2024-01-30 10:38:41 -0300

    Fix totalSupply method

--- wagmi-erc20-hooks/useTotalSupply.ts
+++ wagmi-erc20-hooks/useTotalSupply.ts
@@ -5,4 +5,4 @@ import { useReadErc20, type QueryOptions } from './baseErc20'
 export const useTotalSupply = (
   erc20Address: Address,
   { query }: { query?: QueryOptions<bigint> } = {},
-) => useReadErc20(erc20Address, 'allowance', undefined, query)
+) => useReadErc20(erc20Address, 'totalSupply', undefined, query)

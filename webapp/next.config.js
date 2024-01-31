3491e15ebf0a0459bc72d1bcc461fb2d64b15198
Gonzalo D'Elia <gonzalo@bloq.com>
2024-01-31 11:30:20 -0300

    Enable static building

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  transpilePackages: ['wagmi-erc20-hooks'],
   webpack(config) {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     config.externals.push('pino-pretty', 'lokijs', 'encoding')

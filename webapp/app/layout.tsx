5cf8d6d67f952a554c95972414968c392cfd69e4
Gonzalo D'Elia <gonzalo@bloq.com>
2024-02-01 12:23:52 -0300

    Fix client side redirect with locale

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,11 +1,26 @@
+import { locales } from 'app/i18n'
+import type { Metadata } from 'next'
 import { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
 }
 
+export const metadata: Metadata = {
+  title: 'BVM',
+}
+
+export const generateStaticParams = async () =>
+  locales.map(locale => ({ locale }))
+
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
-  return children
+  // Define empty body because otherwise it will break client redirects
+  // See https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
 }

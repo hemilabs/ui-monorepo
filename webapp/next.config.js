f2e1975653f55dcbd5e80e04c768becb9cf20599
Gonzalo D'Elia <gonzalo@bloq.com>
2024-02-07 12:52:38 -0300

    Create ui-common reusable package

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  transpilePackages: ['wagmi-erc20-hooks'],
+  transpilePackages: ['ui-common', 'wagmi-erc20-hooks'],
   webpack(config) {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     config.externals.push('pino-pretty', 'lokijs', 'encoding')

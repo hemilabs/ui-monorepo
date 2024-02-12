815fcd1d4c55bf2f622c52c80d14f260c422844a
Gonzalo D'Elia <gonzalo@bloq.com>
2024-02-12 18:40:37 -0300

    Add hubspot script

--- landing/main.js
+++ landing/main.js
@@ -33,5 +33,15 @@
 
     // on Ready, enable scroll
     enableScroll()
+    // and add the hubspot form
+    // Check https://legacydocs.hubspot.com/docs/methods/forms/advanced_form_options
+    hbspt.forms.create({
+      // required to apply custom styles in globals.css
+      css: '',
+      formId: 'f8a844d5-0803-482d-b43e-23b328a1cdbb',
+      portalId: '21449954',
+      region: 'na1',
+      target: '#hubspot-target-form',
+    })
   })
 })()

85a2522f20a9c8cfb3dd3427677b99f1cf89b3dc
Gonzalo D'Elia <gonzalo@bloq.com>
2024-02-15 15:36:10 -0300

    Add manual configuration section

--- marketing/app/[locale]/network/configureNetworks.tsx
+++ marketing/app/[locale]/network/configureNetworks.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { bridgeableNetworks, hemi } from 'hemi-metadata'
 import Link from 'next/link'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useTranslations } from 'next-intl'
@@ -68,12 +69,93 @@ const Tab = function ({ active, networkConfiguration }: TabProps) {
   )
 }
 
+const ConfigurationPropTitle = ({
+  children,
+  order,
+}: {
+  children: string
+  order: string
+}) => <p className={`text-zinc-500 ${order}`}>{children}</p>
+
+const ExternalLink = ({
+  href,
+  order,
+  ...props
+}: { order: string } & React.DetailedHTMLProps<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>) => (
+  <a
+    className={`cursor-pointer text-rose-400 ${order}`}
+    href={href}
+    rel="noopener noreferrer"
+    target="_blank"
+    {...props}
+  >
+    {href}
+  </a>
+)
+
 const AutomaticConfiguration = function () {
   return <span>Automatic configuration TBD</span>
 }
 
 const ManualConfiguration = function () {
-  return <span>Manual configuration TBD</span>
+  const t = useTranslations('network')
+  const [ethereum] = bridgeableNetworks
+
+  return (
+    <div className="grid grid-cols-1 gap-y-4 rounded-lg bg-slate-100 p-4 text-black xl:grid-cols-2 2xl:grid-cols-3 [&>a]:text-sm [&>h5]:text-base xl:[&>h5]:text-lg [&>p]:text-sm xl:[&>p]:text-base">
+      {/* By not adding order, when visible, this is shown first */}
+      <h5 className="hidden 2xl:block">{t('network-name')}</h5>
+      <h5 className="order-1">{hemi.name}</h5>
+      <ConfigurationPropTitle order="order-2 xl:order-3">
+        {t('rpc-url')}
+      </ConfigurationPropTitle>
+      <ExternalLink
+        href={hemi.rpcUrls.public.http[0]}
+        order="order-2 xl:order-5 2xl:order-3"
+      />
+      <ConfigurationPropTitle order="order-4 xl:order-7">
+        {t('chain-id')}
+      </ConfigurationPropTitle>
+      <p className="order-5 xl:order-9">{hemi.id}</p>
+      <ConfigurationPropTitle order="order-6 xl:order-11">
+        {t('currency-symbol')}
+      </ConfigurationPropTitle>
+      <p className="xl:order-13 order-7">{hemi.nativeCurrency.symbol}</p>
+      <ConfigurationPropTitle order="order-8 xl:order-15">
+        {t('block-explorer-url')}
+      </ConfigurationPropTitle>
+      <ExternalLink
+        href={hemi.blockExplorers.default.url}
+        order="order-9 xl:order-17"
+      />
+      <h5 className="order-10 mt-5 xl:order-2 xl:mt-0">{ethereum.name}</h5>
+      <ConfigurationPropTitle order="order-11 xl:order-8 2xl:hidden">
+        {t('chain-id')}
+      </ConfigurationPropTitle>
+      <p className="order-12 xl:order-10">{ethereum.id}</p>
+      <ConfigurationPropTitle order="order-13 xl:order-4 2xl:hidden">
+        {t('rpc-url')}
+      </ConfigurationPropTitle>
+      <ExternalLink
+        href={ethereum.rpcUrls.default.http[0]}
+        order="order-14 xl:order-6"
+      />
+      <ConfigurationPropTitle order="order-15 xl:order-12 2xl:hidden">
+        {t('currency-symbol')}
+      </ConfigurationPropTitle>
+      <p className="order-16 xl:order-14">{ethereum.nativeCurrency.symbol}</p>
+      <ConfigurationPropTitle order="order-17 xl:order-16 2xl:hidden">
+        {t('block-explorer-url')}
+      </ConfigurationPropTitle>
+      <ExternalLink
+        href={ethereum.blockExplorers.default.url}
+        order="order-18"
+      />
+    </div>
+  )
 }
 
 export const ConfigureNetwork = function () {
@@ -96,9 +178,7 @@ export const ConfigureNetwork = function () {
           />
         </ul>
       </div>
-      {/* some template to show the space the implementation will take */}
-      {/* TODO https://github.com/BVM-priv/ui-monorepo/issues/46 */}
-      <div className="h-[200px]">
+      <div>
         {networkConfiguration === 'automatic' && <AutomaticConfiguration />}
         {networkConfiguration === 'manual' && <ManualConfiguration />}
       </div>

700dc2995f6393b529ac5b7fbf877c1c1eee3fb9
Gonzalo D'Elia <gonzalo@bloq.com>
2024-03-07 11:35:33 -0300

    Update hemi-metadata wagmi,viem

--- hemi-metadata/index.ts
+++ hemi-metadata/index.ts
@@ -1,11 +1,11 @@
-import { Chain } from 'wagmi'
+import { type Chain } from 'viem'
 
 // See https://docs.hemi.xyz/metamask-wallet-setup
-export const hemiTestnet: Chain = {
+export const hemiTestnet = {
   blockExplorers: {
     default: {
       name: 'Hemi Tesnet Explorer',
-      url: 'https://testnet.explorer.hemi.network/',
+      url: 'https://testnet.explorer.hemi.network',
     },
   },
   id: 743_111,
@@ -15,8 +15,6 @@ export const hemiTestnet: Chain = {
     name: 'Testnet Hemi Ether',
     symbol: 'thETH',
   },
-  // This field will be deprecated in viem v2, but it's required for now
-  network: 'sepolia',
   rpcUrls: {
     default: {
       http: ['https://testnet.rpc.hemi.network/rpc'],
@@ -26,12 +24,13 @@ export const hemiTestnet: Chain = {
     },
   },
   testnet: true,
-}
+} as const satisfies Chain
+// For ↑, See https://wagmi.sh/core/api/chains#create-chain
 
 // Currently, there's no data for mainnet, so let's copy the testnet data
 // Once that's defined, we could use the flag in build time to compile either test or mainnet
 // for exported hemi object
-export const hemiMainnet: Chain = {
+export const hemiMainnet = {
   ...hemiTestnet,
   testnet: false,
-}
+} as const satisfies Chain

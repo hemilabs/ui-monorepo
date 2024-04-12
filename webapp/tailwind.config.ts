fd4cd98e3f5eceed26cbf867bc4a8cec3a9c81a1
Gonzalo D'Elia <gonzalo@bloq.com>
2024-04-12 13:41:24 -0300

    Add tabs component

--- webapp/tailwind.config.ts
+++ webapp/tailwind.config.ts
@@ -18,24 +18,40 @@ const config: Config = {
           'linear-gradient(143deg, #F16063 -3.27%, rgba(116, 96, 241, 0.00) 130.65%)',
         'triangles-pattern': "url('../public/main-background.webp')",
       },
+      // See https://www.figma.com/file/4fVd9wneclsvYDYD95ApZ9/Hemi-Portal?node-id=8%3A5122&mode=dev
       colors: {
-        gray: {
-          1: '#E5E6E6',
-          3: '#CCCCCC',
-          5: '#8F8F8F',
-          6: '#808080',
-          9: '#1A1C20',
-        },
         orange: {
           1: '#FF4D00',
+          50: '#FFFBF8',
+          200: '#FFE2D0',
+          300: '#FFD3B9',
+          400: '#FFC4A1',
+          500: '#FFB58A',
+          600: '#FFA773',
+          700: '#FF985B',
+          800: '#FF7D33',
+          900: '#FF7B2C',
+          950: '#FF6C15',
           hemi: '#FF5F00',
+        },
+        slate: {
+          100: '#E5E6E6',
+          200: '#CCCCCC',
+          300: '#B2B3B3',
+          400: '#999A9A',
+          500: '#808080',
+          600: '#666767',
+          700: '#4D4E4E',
+          800: '#333535',
+          900: '#1A1B1B',
+          950: '#000202',
         },
       },
       fontFamily: {
         inter: '--font-inter',
       },
       height: {
-        // 96px from header (height + padding), 40px from container's padding top in > md screns
+        // 96px from header (height + padding), 40px from container's padding top in > md screens
         'fit-rest-screen': 'calc(100dvh - 96px) md:calc(100dvh - 96px - 40px)',
         // 67px from header, 24px from container margin's top, and 28px twice from body y-padding
         'fit-rest-screen-desktop': 'calc(100vh - 67px - 24px - 28px - 28px)',

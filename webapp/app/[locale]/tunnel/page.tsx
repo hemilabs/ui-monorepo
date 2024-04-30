204b85468736904162b8c6d563f01a9da87de48d
Thomas Alvarenga <thomas@bloq.com>
2024-04-30 13:50:28 -0300

    Fix balance text overflow on tunnel page

--- webapp/app/[locale]/tunnel/page.tsx
+++ webapp/app/[locale]/tunnel/page.tsx
@@ -108,37 +108,39 @@ const FormContent = function ({ tunnelState, isRunningOperation }: Props) {
           readonly={fromNetworkId === hemi.id}
         />
       </div>
-      <div className="flex justify-between rounded-xl bg-zinc-50 p-4 text-zinc-400">
-        <div className="flex basis-1/3 flex-col gap-y-2">
-          <span className="text-xs font-normal">{t('form.you-send')}</span>
-          <div className="flex max-w-7 sm:max-w-none">
-            <input
-              className="ml-1 max-w-28 bg-transparent text-base font-medium text-neutral-400"
-              disabled={isRunningOperation}
-              onChange={e => updateFromInput(e.target.value)}
-              type="text"
-              value={fromInput}
+      <div className="flex flex-col justify-between rounded-xl bg-zinc-50 p-4 text-zinc-400">
+        <div className="flex flex-row">
+          <div className="flex basis-1/3 flex-col gap-y-2">
+            <span className="text-xs font-normal">{t('form.you-send')}</span>
+            <div className="flex max-w-7 sm:max-w-none">
+              <input
+                className="ml-1 max-w-28 bg-transparent text-base font-medium text-neutral-400"
+                disabled={isRunningOperation}
+                onChange={e => updateFromInput(e.target.value)}
+                type="text"
+                value={fromInput}
+              />
+            </div>
+          </div>
+          <div className="flex basis-2/3 flex-col justify-between gap-y-3">
+            <TokenSelector
+              onSelectToken={updateFromToken}
+              selectedToken={fromToken}
+              tokens={tokenList.tokens.filter(
+                token => token.chainId === fromNetworkId,
+              )}
             />
           </div>
         </div>
-        <div className="flex basis-2/3 flex-col justify-between gap-y-3">
-          <TokenSelector
-            onSelectToken={updateFromToken}
-            selectedToken={fromToken}
-            tokens={tokenList.tokens.filter(
-              token => token.chainId === fromNetworkId,
-            )}
+        <div className="flex items-center justify-end gap-x-2 text-xs font-normal sm:text-sm">
+          {t('form.balance')}: <Balance token={fromToken} />
+          <SetMaxBalance
+            fromToken={fromToken}
+            isRunningOperation={isRunningOperation}
+            onSetMaxBalance={maxBalance =>
+              updateFromInput(formatNumber(maxBalance, 2))
+            }
           />
-          <div className="flex items-center justify-end gap-x-2 text-xs font-normal sm:text-sm">
-            {t('form.balance')}: <Balance token={fromToken} />
-            <SetMaxBalance
-              fromToken={fromToken}
-              isRunningOperation={isRunningOperation}
-              onSetMaxBalance={maxBalance =>
-                updateFromInput(formatNumber(maxBalance, 2))
-              }
-            />
-          </div>
         </div>
       </div>
       <div className="mx-auto flex h-10">
@@ -153,26 +155,28 @@ const FormContent = function ({ tunnelState, isRunningOperation }: Props) {
           readonly={toNetworkId === hemi.id}
         />
       </div>
-      <div className="flex justify-between rounded-xl bg-zinc-50 p-4 text-zinc-400">
-        <div className="flex flex-col gap-y-2">
-          <span className="text-xs font-normal">{t('form.you-receive')}</span>
-          <div className="flex items-center gap-x-2">
-            <span className="text-base font-medium text-neutral-400">
-              {/* Bridging goes 1:1, so output equals input */}
-              <span className="ml-1">{fromInput}</span>
-            </span>
+      <div className="flex flex-col justify-between rounded-xl bg-zinc-50 p-4 text-zinc-400">
+        <div className="flex flex-row">
+          <div className="flex basis-1/3 flex-col gap-y-2">
+            <span className="text-xs font-normal">{t('form.you-receive')}</span>
+            <div className="flex items-center gap-x-2">
+              <span className="text-base font-medium text-neutral-400">
+                {/* Bridging goes 1:1, so output equals input */}
+                <span className="ml-1">{fromInput}</span>
+              </span>
+            </div>
+          </div>
+          <div className="flex basis-2/3 justify-end gap-y-3">
+            <div className="flex items-center justify-end gap-x-2 text-xs">
+              <TokenLogo token={toToken} />
+              <span className="text-sm font-medium text-slate-700">
+                {toToken.symbol}
+              </span>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between gap-y-3">
-          <div className="flex items-center justify-end gap-x-2 text-xs">
-            <TokenLogo token={toToken} />
-            <span className="text-sm font-medium text-slate-700">
-              {toToken.symbol}
-            </span>
-          </div>
-          <div className="flex items-center justify-end gap-x-2 text-sm font-normal">
-            {t('form.balance')}: <Balance token={toToken} />
-          </div>
+        <div className="flex items-center justify-end gap-x-2 text-sm font-normal">
+          {t('form.balance')}: <Balance token={toToken} />
         </div>
       </div>
     </>

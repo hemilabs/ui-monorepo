ce1a46162058e989dc05f2fe8e5c6ea7c6a3d497
arturdolzan <arturdolzan@gmail.com>
2024-05-03 10:20:36 -0300

    Implement paginator component

--- ui-common/components/tabs.tsx
+++ ui-common/components/tabs.tsx
@@ -3,7 +3,7 @@
 import Link from 'next-intl/link'
 import { ReactNode } from 'react'
 
-type Button = { onClick: () => void }
+type Button = { onClick?: () => void }
 type Anchor = { href: string }
 
 type TabProps = {
@@ -12,10 +12,8 @@ type TabProps = {
   selected?: boolean
 } & (Anchor | Button)
 
-// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-function tabIsButton(value: Button | Anchor): value is Button {
-  return (value as Button).onClick !== undefined
-}
+const tabIsButton = (value: Button | Anchor): value is Button =>
+  (value as Button).onClick !== undefined
 
 export const Tab = function ({
   children,
@@ -24,22 +22,23 @@ export const Tab = function ({
   ...props
 }: TabProps) {
   const isButton = tabIsButton(props)
-  const className = `px-6 py-2 text-sm font-medium inline-block ${
+  const className = `px-5 md:px-6 py-2 text-sm font-medium inline-block ${
     selected ? 'text-orange-950' : 'text-slate-300'
-  }`
+  }
+  ${disabled ? 'opacity-40' : 'opacity-100'}`
   return (
     <li className="border-y border-l border-solid border-slate-100 bg-white shadow-sm first:rounded-l-xl last:rounded-r-xl last:border-r">
-      {isButton && (
+      {(isButton || disabled) && (
         <button
           className={className}
           disabled={disabled || selected}
-          onClick={props.onClick}
+          onClick={isButton && !disabled ? props.onClick : undefined}
           type="button"
         >
           {children}
         </button>
       )}
-      {!isButton && (
+      {!isButton && props.href && (
         <Link className={className} href={props.href}>
           {children}
         </Link>

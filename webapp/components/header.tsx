9bfab48d7203a1537e07290269806aa3da8161c2
Gonzalo D'Elia <gonzalo@bloq.com>
2024-05-08 11:56:34 -0300

    Remove old swap page and links

--- webapp/components/header.tsx
+++ webapp/components/header.tsx
@@ -2,8 +2,6 @@
 
 import { navItems } from 'app/[locale]/navbar/navData'
 import dynamic from 'next/dynamic'
-import { usePathname } from 'next/navigation'
-import { useTranslations } from 'next-intl'
 import Link from 'next-intl/link'
 import { useState } from 'react'
 import Skeleton from 'react-loading-skeleton'
@@ -35,29 +33,8 @@ const WalletConnectMobile = dynamic(
   },
 )
 
-type Props = {
-  path: '/swap' | '/tunnel'
-  text: string
-}
-
-const Route = function ({ path, text }: Props) {
-  const pathname = usePathname()
-  const activeLink = 'bg-white text-black cursor-auto'
-  const inactiveLink = 'bg-transparent cursor-pointer text-zinc-500'
-  return (
-    <li
-      className={`rounded-2xl px-5 py-3 ${
-        pathname === path ? activeLink : inactiveLink
-      }`}
-    >
-      <Link href={path}>{text}</Link>
-    </li>
-  )
-}
-
 export const Header = function () {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const t = useTranslations('common')
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
 
@@ -74,19 +51,6 @@ export const Header = function () {
           <HemiSymbol />
         </Link>
       </div>
-      {/* Hidden below until links are re-enabled */}
-      {/* <div className="fixed bottom-0 left-0 right-0 z-10 h-[90px] bg-white/60 backdrop-blur-sm md:hidden" /> */}
-      <div className="hidden">
-        <div className="fixed bottom-0 z-10 flex w-full pb-6 pt-4 md:absolute md:left-1/2 md:block md:-translate-x-1/2">
-          <ul className="mx-auto hidden justify-center gap-x-4 rounded-xl bg-neutral-100 p-1 md:mx-0 md:bg-inherit">
-            <Route path="/tunnel" text={t('tunnel')} />
-            <Route path="/swap" text={t('swap')} />
-          </ul>
-        </div>
-      </div>
-      {/* Hidden above until links are re-enabled */}
-      {/* <WalletConnectButton /> */}
-
       <button
         className="ml-auto mr-4 cursor-pointer md:hidden"
         onClick={toggleMenu}

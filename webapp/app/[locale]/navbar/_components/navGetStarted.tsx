45590a314c3bfe8b5c108545f770bc6563b82029
Gonzalo D'Elia <gonzalo@bloq.com>
2024-05-09 17:43:48 -0300

    Cleanup unused svg component

--- webapp/app/[locale]/navbar/_components/navGetStarted.tsx
+++ webapp/app/[locale]/navbar/_components/navGetStarted.tsx
@@ -4,39 +4,6 @@ type Props = {
   children: React.ReactNode
 }
 
-const SvgComponent = props => (
-  <svg
-    fill="none"
-    height={121}
-    width={180}
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path d="M0 .5h180v120H0z" fill="url(#a)" />
-    <path d="M147 48.5h36v36h-36zM39-23.422h36v36H39z" fill="#fff" />
-    <path d="M110.7 48.5h36v36h-36zM38.7 12.5h36v36h-36z" fill="#FF6C15" />
-    <path
-      d="M-33-23.5H3v36h-36zM-33 12.5H3v36h-36zM-33 48.5H3v36h-36zM-33 84.5H3v36h-36zM3-23.5h36v36H3zM3 12.5h36v36H3zM3 48.5h36v36H3zM3 84.5h36v36H3zM39-23.5h36v36H39zM39 12.5h36v36H39zM39 48.5h36v36H39zM39 84.5h36v36H39zM75-23.5h36v36H75zM75 12.5h36v36H75zM75 48.5h36v36H75zM75 84.5h36v36H75zM111-23.5h36v36h-36zM111 12.5h36v36h-36zM111 48.5h36v36h-36zM111 84.5h36v36h-36zM147-23.5h36v36h-36zM147 12.5h36v36h-36zM147 48.5h36v36h-36zM147 84.5h36v36h-36z"
-      stroke="#FF6C15"
-    />
-    <path d="M.5 1h179v119H.5z" stroke="#E5E6E6" />
-    <defs>
-      <linearGradient
-        gradientUnits="userSpaceOnUse"
-        id="a"
-        x1={90}
-        x2={90}
-        y1={0.5}
-        y2={120.5}
-      >
-        <stop />
-        <stop offset={1} />
-      </linearGradient>
-    </defs>
-  </svg>
-)
-export default SvgComponent
-
 export const NavGetStarted = ({ children }: Props) => (
   <div
     className="h-30 w-45 relative cursor-pointer 

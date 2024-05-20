2588d6e32b73d51e7a0f33ddeff08cbcccb5c31d
arturdolzan <arturdolzan@gmail.com>
2024-05-20 18:45:31 -0300

    Fix get started hemi modal for responsive layouts

--- webapp/app/[locale]/get-started/selectProfile.tsx
+++ webapp/app/[locale]/get-started/selectProfile.tsx
@@ -35,39 +35,46 @@ export const SelectProfile = function () {
 
   return (
     <Modal>
-      <div className="h-3/4 w-full">
-        <Card padding="medium" radius="large" shadow="regular">
-          <div className="flex flex-col gap-y-7 overflow-y-auto text-center md:max-h-none">
-            <h3 className="text-2xl font-medium tracking-tighter text-slate-900 lg:text-3xl">
-              {t('heading')}
-            </h3>
-            <p className="text-base text-slate-500 lg:text-lg">
-              {t('sub-heading')}
-            </p>
-            <div className="flex flex-col items-center gap-y-6 md:flex-row md:gap-x-6">
-              {options.map(({ icon, label, profile }) => (
-                <Link
-                  href={`${pathname}${
-                    searchParams.size > 0 ? `?${searchParams.toString()}&` : '?'
-                  }profile=${profile}`}
-                  key={profile}
-                  onClick={() =>
-                    localStorage.setItem('portal.get-started-profile', profile)
-                  }
-                >
-                  <div className="w-48 md:w-52">
-                    <Card borderColor="gray" padding="medium">
-                      <div className="flex flex-col items-center gap-y-5 md:justify-end lg:h-52">
-                        <div className="md:mb-7 md:mt-auto">{icon}</div>
-                        <Button variant="tertiary">{label}</Button>
-                      </div>
-                    </Card>
-                  </div>
-                </Link>
-              ))}
+      <div className="flex h-screen items-center justify-center md:h-auto md:min-h-0">
+        <div className="w-full">
+          <Card padding="medium" radius="large" shadow="regular">
+            <div className="flex flex-col gap-y-4 overflow-y-auto text-center md:max-h-none md:gap-y-7">
+              <h3 className="text-2xl font-medium tracking-tighter text-slate-900 lg:text-3xl">
+                {t('heading')}
+              </h3>
+              <p className="text-base text-slate-500 lg:text-lg">
+                {t('sub-heading')}
+              </p>
+              <div className="flex flex-col items-center gap-y-6 md:flex-row md:gap-x-6">
+                {options.map(({ icon, label, profile }) => (
+                  <Link
+                    href={`${pathname}${
+                      searchParams.size > 0
+                        ? `?${searchParams.toString()}&`
+                        : '?'
+                    }profile=${profile}`}
+                    key={profile}
+                    onClick={() =>
+                      localStorage.setItem(
+                        'portal.get-started-profile',
+                        profile,
+                      )
+                    }
+                  >
+                    <div className="w-48 md:w-52">
+                      <Card borderColor="gray" padding="medium">
+                        <div className="flex flex-col items-center gap-y-2 md:justify-end md:gap-y-5 lg:h-52">
+                          <div className="md:mb-7 md:mt-auto">{icon}</div>
+                          <Button variant="tertiary">{label}</Button>
+                        </div>
+                      </Card>
+                    </div>
+                  </Link>
+                ))}
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
+        </div>
       </div>
     </Modal>
   )

a1f1088c2a035c683f1f0869725b50e73520d5b0
Gonzalo D'Elia <gonzalo@bloq.com>
2024-06-19 18:45:56 -0300

    Move check mark into svg icon component

---
+++ webapp/components/icons/checkMark.tsx
@@ -0,0 +1,11 @@
+export const CheckMark = () => (
+  <svg fill="none" height={12} width={12} xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M1.375 7.547 4.5 10.125l6.125-8.25"
+      stroke="#FF6C15"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+    />
+  </svg>
+)

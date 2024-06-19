b0f57549fc4311b9437e41f9dfcdc00e53170a4e
Gonzalo D'Elia <gonzalo@bloq.com>
2024-06-19 18:44:48 -0300

    Remove unused prop in menu.tsx

--- webapp/components/menu.tsx
+++ webapp/components/menu.tsx
@@ -1,13 +1,9 @@
 type Props = {
   items: { id: string; content: React.ReactNode }[]
-  refProp?: React.MutableRefObject<HTMLDivElement>
 }
 
-export const Menu = ({ items, refProp }: Props) => (
-  <div
-    className="absolute bottom-0 right-0 z-10 translate-y-[calc(100%+5px)] rounded-lg border border-solid border-slate-600/45 bg-white p-1 text-sm shadow-sm"
-    ref={refProp}
-  >
+export const Menu = ({ items }: Props) => (
+  <div className="absolute bottom-0 right-0 z-10 translate-y-[calc(100%+5px)] rounded-lg border border-solid border-slate-600/45 bg-white p-1 text-sm shadow-sm">
     <ul className="flex flex-col gap-y-1">
       {items.map(({ content, id }) => (
         <li

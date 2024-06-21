0966b5bba1e090fb937bb391f8d5865b883fca6f
Gonzalo D'Elia <gonzalo@bloq.com>
2024-06-21 17:59:21 -0300

    Simplify btc hash regex check

--- btc-wallet/utils/hash.ts
+++ btc-wallet/utils/hash.ts
@@ -1,4 +1,4 @@
 export type BtcTxHash = string
 
 export const isBtcTxHash = (value: string): value is BtcTxHash =>
-  /^[0-9a-fA-F]*$/.test(value) && value.length === 64
+  /^[0-9a-fA-F]{64}$/.test(value)

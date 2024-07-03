6990ddb2405837df66029287b1cbf7a7c3024e6d
Gonzalo D'Elia <gonzalo@bloq.com>
2024-07-03 15:24:48 -0300

    Add block height and timestamp for btc txs

--- webapp/hooks/useWaitForTransactionReceipt.ts
+++ webapp/hooks/useWaitForTransactionReceipt.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import { useAccount } from 'btc-wallet/hooks/useAccount'
 import { type BtcTransaction } from 'btc-wallet/unisat'
 import { isChainIdSupported } from 'btc-wallet/utils/chains'
+import camelCaseKeys from 'camelcase-keys'
 import fetch from 'fetch-plus-plus'
 
 type Args = {
@@ -11,6 +12,8 @@ type Args = {
 // See https://mempool.space/testnet/docs/api/rest#get-transaction
 type TransactionReceipt = {
   status: {
+    blockHeight?: number
+    blockTime?: number
     confirmed: boolean
   }
 }
@@ -24,8 +27,8 @@ export const useWaitForTransactionReceipt = function ({ txId }: Args) {
     ...useQuery({
       enabled: !!txId && !!chainId && isChainIdSupported(chainId),
       queryFn: () =>
-        fetch(`${process.env.NEXT_PUBLIC_MEMPOOL_API_URL}/tx/${txId}`).catch(
-          function (err) {
+        fetch(`${process.env.NEXT_PUBLIC_MEMPOOL_API_URL}/tx/${txId}`)
+          .catch(function (err) {
             if (err?.message.includes('not found')) {
               // It seems it takes a couple of seconds for the Tx for being picked up
               // react-query doesn't let us to return undefined data, so we must
@@ -35,8 +38,8 @@ export const useWaitForTransactionReceipt = function ({ txId }: Args) {
               return { status: { confirmed: false } }
             }
             throw err
-          },
-        ) as Promise<TransactionReceipt | undefined>,
+          })
+          .then(camelCaseKeys) as Promise<TransactionReceipt | undefined>,
       queryKey,
       refetchInterval(query) {
         // Poll every 30 secs until confirmed

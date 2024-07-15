2b5a145bb6baec930436e6f69466c4a0a798af2b
Gonzalo D'Elia <gonzalo@bloq.com>
2024-07-14 22:06:46 -0300

    Update time ago in tx history

--- webapp/app/[locale]/tunnel/transaction-history/_components/timeAgo.tsx
+++ webapp/app/[locale]/tunnel/transaction-history/_components/timeAgo.tsx
@@ -1,9 +1,47 @@
 import JsTimeAgo from 'javascript-time-ago'
 import enLocale from 'javascript-time-ago/locale/en'
 import esLocale from 'javascript-time-ago/locale/es'
+import { useEffect, useMemo, useState } from 'react'
 
 const modules = [enLocale, esLocale]
 modules.forEach(module => JsTimeAgo.addLocale(module))
+
+const second = 1
+const minute = second * 60
+const hour = minute * 60
+
+const toMs = (seconds: number) => seconds * 1000
+const toSeconds = (minutes: number) => minutes * 60
+const toMinutes = (hours: number) => hours * 60
+
+const getTimeoutInterval = function (timestamp: number, now: number) {
+  const difference = now - timestamp
+  // render every second if less than 60 seconds
+  if (difference <= toMs(second * 60)) {
+    return toMs(second)
+  }
+  // render every minute if less than one hour
+  if (difference <= toMs(toSeconds(toMinutes(hour)))) {
+    return toMs(toSeconds(minute))
+  }
+  // render once an hour for the rest, we don't care if we force render every hour
+  // if the user stares at the screen for one day.
+  return toMs(toSeconds(toMinutes(hour)))
+}
+
+const useRerender = function (timestamp: number) {
+  const [now, setNow] = useState(new Date().getTime())
+  useEffect(
+    function forceDateUpdate() {
+      const timeoutId = setTimeout(
+        () => setNow(new Date().getTime()),
+        getTimeoutInterval(timestamp, now),
+      )
+      return () => clearTimeout(timeoutId)
+    },
+    [now, setNow, timestamp],
+  )
+}
 
 type Props = {
   locale: string
@@ -11,7 +49,15 @@ type Props = {
 }
 
 export const TimeAgo = function ({ locale, timestamp }: Props) {
-  const timeAgo = new JsTimeAgo(locale)
+  const timeAgo = useMemo(() => new JsTimeAgo(locale), [locale])
+
   // timestamp is unix format
-  return timeAgo.format(timestamp * 1000)
+  const date = timestamp * 1000
+  const formattedDate = timeAgo.format(date, 'round')
+
+  // force rerender depending on how old the timestamp is
+  // for better ux
+  useRerender(date)
+
+  return <>{formattedDate}</>
 }

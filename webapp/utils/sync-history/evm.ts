b3e86dbd61fe600f344cd7f0b51bb6c67599c3d8
Gonzalo D'Elia <gonzalo@bloq.com>
2024-08-14 19:29:38 -0300

    Move getPayload fn into its own file

--- webapp/utils/sync-history/evm.ts
+++ webapp/utils/sync-history/evm.ts
@@ -19,11 +19,11 @@ import { getEvmBlock } from 'utils/evmApi'
 import { createPublicProvider } from 'utils/providers'
 import { type Chain } from 'viem'
 
+import { getPayload } from './common'
 import {
   type EvmSyncParameters,
   type ExtendedSyncInfo,
   type SaveHistory,
-  type SyncInfo,
 } from './types'
 
 const toOperation =
@@ -71,32 +71,6 @@ export const createEvmSync = function ({
       blockNumber,
     )
     return blockNumber
-  }
-
-  const getPayload = function ({
-    canMove,
-    fromBlock,
-    lastBlock,
-    nextState,
-  }: {
-    canMove: boolean
-    fromBlock: SyncInfo['fromBlock']
-    lastBlock: number
-    nextState: Parameters<CreateSlidingBlockWindow['onChange']>[0]['nextState']
-  }) {
-    const hasSyncToMinBlock = !canMove
-    return {
-      // if we finished, we should start from the beginning the next time with the new values
-      chunkIndex: hasSyncToMinBlock ? 0 : nextState.windowIndex,
-      // If we finished syncing, the upper bound is the last block we've synced up to
-      // so next time we should start from that block + 1 (the following one).
-      // If we haven't finished, we keep the same value
-      fromBlock: hasSyncToMinBlock ? lastBlock + 1 : fromBlock,
-      hasSyncToMinBlock,
-      // if we finished synced, the next "toBlock" value will be retrieved
-      // in runtime, so we must clear it. Otherwise, keep the existing value
-      toBlock: hasSyncToMinBlock ? undefined : lastBlock,
-    }
   }
 
   const syncDeposits = async function (

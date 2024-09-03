8c23273e70814aeae2b1744f7057bc60cd4ff303
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-03 15:43:58 -0300

    Update icons names and definitions

---
+++ webapp/components/icons/bitcoinKit.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps } from 'react'
+
+export const BitcoinKitIcon = (props: ComponentProps<'svg'>) => (
+  <svg
+    fill="none"
+    height={14}
+    width={14}
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      clipRule="evenodd"
+      d="M2.975 1.4A1.575 1.575 0 0 0 1.4 2.975v8.05A1.575 1.575 0 0 0 2.975 12.6h8.05a1.575 1.575 0 0 0 1.575-1.575v-8.05A1.575 1.575 0 0 0 11.025 1.4h-8.05Zm2.821 4.396a.525.525 0 0 0-.742-.742L3.48 6.63a.525.525 0 0 0 0 .742l1.575 1.575a.525.525 0 0 0 .742-.742L4.592 7l1.204-1.204Zm3.15-.742a.525.525 0 1 0-.742.742L9.408 7 8.204 8.204a.525.525 0 1 0 .742.742l1.575-1.575a.525.525 0 0 0 0-.742L8.946 5.054Z"
+      fill="#A3A3A3"
+      fillRule="evenodd"
+    />
+  </svg>
+)

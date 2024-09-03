de19533ccc16289033720baa748831b5f61c2440
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-03 15:43:58 -0300

    Update icons names and definitions

--- webapp/components/icons/electroCardiogramIcon.tsx
+++ webapp/components/icons/electroCardiogramIcon.tsx
@@ -1,17 +1,19 @@
-export const ElectroCardiogramIcon = (props: React.SVGProps<SVGSVGElement>) => (
+import { ComponentProps } from 'react'
+
+export const ElectroCardiogramIcon = (props: ComponentProps<'svg'>) => (
   <svg
     fill="none"
-    height="22"
-    viewBox="0 0 22 22"
-    width="22"
+    height={14}
+    width={14}
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <path
-      d="M1.5 9.5h3l2.25-6 4.5 12 2.25-6h3"
-      stroke="currentColor"
-      strokeLinecap="square"
-      strokeWidth={1.5}
+      d="M1.167 7h1.906c.254 0 .48-.165.556-.408L5.111 1.9a.146.146 0 0 1 .278 0L8.611 12.1a.146.146 0 0 0 .278 0l1.483-4.693A.583.583 0 0 1 10.928 7h1.906"
+      stroke="#A3A3A3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.167}
     />
   </svg>
 )

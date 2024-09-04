e78131af1b15e4a5cd131aee0a60aac3cfb84810
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-04 15:24:29 -0300

    Implement navbar desktop + mobile fixes

--- webapp/app/[locale]/_components/navbar/_components/socialLinks.tsx
+++ webapp/app/[locale]/_components/navbar/_components/socialLinks.tsx
@@ -9,7 +9,7 @@ import React from 'react'
 const { discordUrl, twitterUrl, githubUrl, linkedinUrl } = hemiSocials
 
 export const SocialLinks = () => (
-  <div className="mb-1 mt-4 flex items-center justify-center gap-x-6 rounded-lg bg-transparent md:h-14 md:gap-x-3 md:bg-neutral-50">
+  <div className="mb-1 mt-4 flex items-center justify-center gap-x-6 rounded-lg bg-transparent md:h-14 md:gap-x-4 md:bg-neutral-50">
     <ExternalLink href={twitterUrl}>
       <TwitterIcon className="scale-125" />
     </ExternalLink>

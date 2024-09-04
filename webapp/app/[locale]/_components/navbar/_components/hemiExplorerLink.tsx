e995b8f2927496f6a9c0d1aa685b4569495e6389
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-04 11:04:42 -0300

    Implement navbar mobile

---
+++ webapp/app/[locale]/_components/navbar/_components/hemiExplorerLink.tsx
@@ -0,0 +1,18 @@
+import { ExplorerIcon } from 'components/icons/explorerIcon'
+import { useHemi } from 'hooks/useHemi'
+import { useTranslations } from 'next-intl'
+
+import { ItemLink } from './navItem'
+
+export const HemiExplorerLink = function () {
+  const hemi = useHemi()
+  const t = useTranslations('navbar')
+
+  return (
+    <ItemLink
+      href={hemi.blockExplorers.default.url}
+      icon={<ExplorerIcon />}
+      text={t('explorer')}
+    />
+  )
+}

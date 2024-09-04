87adbd59922e6bae153f458dbe32310a60106238
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-04 17:33:16 -0300

    Apply fixes from round of testing and feedback

--- webapp/app/[locale]/_components/navbar/_components/navItem.tsx
+++ webapp/app/[locale]/_components/navbar/_components/navItem.tsx
@@ -4,7 +4,7 @@ import { Chevron } from 'components/icons/chevron'
 import { usePathname } from 'next/navigation'
 import { useLocale } from 'next-intl'
 import Link from 'next-intl/link'
-import { ComponentProps, ReactNode, useState } from 'react'
+import { ComponentProps, MutableRefObject, ReactNode, useState } from 'react'
 import { useOnClickOutside } from 'ui-common/hooks/useOnClickOutside'
 import { isRelativeUrl } from 'utils/url'
 
@@ -21,8 +21,8 @@ const IconContainer = ({
   selected = false,
 }: Selectable & { children: ReactNode }) => (
   <div
-    className={`flex h-5 w-5 items-center 
-    justify-center rounded-[4px] border py-1 shadow-md ${
+    className={`flex h-6 w-6 items-center justify-center rounded-[4px] 
+    border py-1 shadow-md md:h-5 md:w-5 ${
       selected
         ? 'border-orange-700/55 bg-orange-500 [&>svg>path]:fill-white'
         : 'border-neutral-300/55 bg-stone-50'
@@ -43,11 +43,31 @@ const ItemContainer = ({
 }: { children: ReactNode } & Selectable & ComponentProps<'div'>) => (
   <div
     {...props}
-    className={`cursor-pointer rounded-lg py-2 transition-colors duration-300 ${
+    className={`group/item cursor-pointer rounded-lg py-2 transition-colors duration-300 ${
       selected
         ? 'border border-orange-500/55 bg-orange-50'
-        : 'hover:bg-neutral-200'
+        : 'border border-transparent hover:bg-neutral-100'
     }`}
+  >
+    {children}
+  </div>
+)
+
+const MenuContainer = ({
+  children,
+  isOpen,
+  refProp,
+  ...props
+}: { children: ReactNode } & {
+  isOpen: boolean
+  refProp: MutableRefObject<HTMLDivElement>
+} & ComponentProps<'div'>) => (
+  <div
+    {...props}
+    className={`cursor-pointer rounded-lg py-2 transition-colors duration-300 ${
+      isOpen ? 'rounded-lg bg-neutral-100' : 'hover:bg-neutral-100'
+    }`}
+    ref={refProp}
   >
     {children}
   </div>
@@ -58,9 +78,10 @@ const ItemText = ({
   text,
 }: Pick<Props, 'text'> & Selectable) => (
   <span
-    className={`text-xs font-medium text-neutral-600 transition-colors duration-300 hover:text-neutral-950 md:text-sm ${
-      selected ? 'text-neutral-950' : 'text-neutral-600'
-    }`}
+    className={`md:text-ms text-base font-medium leading-5 transition-colors
+      duration-300 group-hover/item:text-neutral-950 ${
+        selected ? 'text-neutral-950' : 'text-neutral-600'
+      }`}
   >
     {text}
   </span>
@@ -89,7 +110,7 @@ const PageLink = function ({ href, icon, rightSection, text }: ItemLinkProps) {
       <Link href={href}>
         <Row>
           <IconContainer selected={selected}>{icon}</IconContainer>
-          <ItemText text={text} />
+          <ItemText selected={selected} text={text} />
           {rightSection}
         </Row>
       </Link>
@@ -111,25 +132,20 @@ export const ItemWithSubmenu = function ({
 }: Props & { subMenu: ReactNode }) {
   const [isOpen, setIsOpen] = useState(false)
 
-  const ref = useOnClickOutside<HTMLDivElement>()
+  const ref = useOnClickOutside<HTMLDivElement>(() => setIsOpen(false))
 
   return (
-    <ItemContainer>
-      <div
-        className={isOpen ? 'rounded-lg hover:bg-neutral-200' : undefined}
-        ref={ref}
-      >
-        <Row onClick={() => setIsOpen(!isOpen)}>
-          <IconContainer>{icon}</IconContainer>
-          <ItemText text={text} />
-          {isOpen ? (
-            <Chevron.Up className="-mr-1 ml-auto" />
-          ) : (
-            <Chevron.Bottom className="-mr-1 ml-auto" />
-          )}
-        </Row>
-      </div>
-      {isOpen && subMenu}
-    </ItemContainer>
+    <MenuContainer isOpen={isOpen} refProp={ref}>
+      <Row onClick={() => setIsOpen(!isOpen)}>
+        <IconContainer>{icon}</IconContainer>
+        <ItemText selected={isOpen} text={text} />
+        {isOpen ? (
+          <Chevron.Up className="-mr-1 ml-auto" />
+        ) : (
+          <Chevron.Bottom className="-mr-1 ml-auto" />
+        )}
+      </Row>
+      {isOpen && <ul className="mt-2 flex flex-col">{subMenu}</ul>}
+    </MenuContainer>
   )
 }
--- webapp/app/[locale]/_components/navbar/_components/termsAndConditions.tsx
+++ webapp/app/[locale]/_components/navbar/_components/termsAndConditions.tsx
@@ -1,12 +1,12 @@
 import { ExternalLink } from 'components/externalLink'
 import { useTranslations } from 'next-intl'
 
-const cssLink = 'text-orange-500'
+const cssLink = 'text-orange-500 hover:text-orange-700'
 
 export const TermsAndConditions = function () {
   const t = useTranslations('navbar')
   return (
-    <p className="font-base mx-auto w-3/4 text-center text-xs text-neutral-400 md:w-full md:text-left">
+    <p className="font-base mx-auto w-3/4 pb-4 text-center text-xs text-neutral-400 md:w-full md:pb-0 md:text-left">
       {t.rich('agree-to-terms-and-policy', {
         policy: (chunk: string) => (
           <ExternalLink

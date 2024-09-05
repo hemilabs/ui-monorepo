a40e23d535faf6b45d5c0310a9250a9430aaa4b1
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-05 14:41:49 -0300

    Add dropdown to switch testnet|mainnet

--- webapp/components/icons/checkMark.tsx
+++ webapp/components/icons/checkMark.tsx
@@ -1,5 +1,13 @@
-export const CheckMark = () => (
-  <svg fill="none" height={12} width={12} xmlns="http://www.w3.org/2000/svg">
+import { ComponentProps } from 'react'
+
+export const CheckMark = (props: ComponentProps<'svg'>) => (
+  <svg
+    fill="none"
+    height={12}
+    width={12}
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
     <path
       d="M1.375 7.547 4.5 10.125l6.125-8.25"
       stroke="#FF6C15"

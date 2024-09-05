de1b3c0a27c4478d61641062d1f94a5583a92ed4
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-05 15:42:41 -0300

    Add border testnet indicator

--- webapp/app/[locale]/_components/appLayout.tsx
+++ webapp/app/[locale]/_components/appLayout.tsx
@@ -30,15 +30,31 @@ export const AppLayout = function ({ children }: Props) {
 
   return (
     <div
-      className={`bg-hemi-layout bg-hemi-color-layout shadow-hemi-layout backdrop-blur-20 lg:100-dvh lg:h-97vh
-    flex w-3/4 flex-1 flex-col self-stretch overflow-y-hidden border-slate-100
-    md:my-3 md:mr-2 md:w-[calc(75%-8px)] md:rounded-3xl md:border md:pb-0`}
+      className={`shadow-hemi-layout backdrop-blur-20 lg:100-dvh
+        lg:h-97vh flex w-3/4 flex-1 flex-col self-stretch overflow-y-hidden bg-neutral-50
+    ${
+      networkType === 'testnet'
+        ? 'md:border-2 md:border-solid md:border-orange-500'
+        : 'md:border'
+    }
+    md:my-3 md:mr-2 md:w-[calc(75%-8px)] md:rounded-2xl md:pb-0`}
     >
       <Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
       <div
-        className={`max-h-[calc(100vh-1rem)] flex-grow overflow-y-auto px-5 pt-4 ${hiddenClass} pb-3`}
+        className={`max-h-[calc(100vh-1rem)] flex-grow
+          ${
+            networkType === 'testnet'
+              ? 'max-md:border-3  max-md:border-solid max-md:border-orange-500'
+              : ''
+          }
+        overflow-y-auto px-5 pt-4
+        ${hiddenClass} pb-3`}
       >
         {children}
+      </div>
+      <div className="h-14 md:hidden">
+        {/* See https://github.com/hemilabs/ui-monorepo/issues/502 */}
+        <footer>Footer</footer>
       </div>
       {isMenuOpen && (
         <div className="md:hidden">

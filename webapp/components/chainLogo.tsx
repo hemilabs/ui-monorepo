c233b89564c1994f918281b9c622e1b105f461d7
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-09 14:44:19 -0300

    Replace eth/hemi/btc logos

--- webapp/components/chainLogo.tsx
+++ webapp/components/chainLogo.tsx
@@ -1,7 +1,7 @@
 import { bitcoinTestnet, bitcoinMainnet } from 'btc-wallet/chains'
+import { BtcLogo } from 'components/icons/btcLogo'
 import { hemi as hemiMainnet, hemiSepolia as hemiSepolia } from 'hemi-viem'
 import { type RemoteChain } from 'types/chain'
-import { BtcLogo } from 'ui-common/components/btcLogo'
 import { EthLogo } from 'ui-common/components/ethLogo'
 import { HemiTokenWithBackground } from 'ui-common/components/hemiLogo'
 import { mainnet, sepolia } from 'viem/chains'

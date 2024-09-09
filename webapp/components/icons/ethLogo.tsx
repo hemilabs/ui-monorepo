3e82dfef9191cf7661a855431d44911d7dcb806c
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-09 14:43:38 -0300

    Add chain selector in footer

---
+++ webapp/components/icons/ethLogo.tsx
@@ -0,0 +1,36 @@
+import { renderToString } from 'react-dom/server'
+
+export const EthLogo = () => (
+  <svg fill="none" height={20} width={20} xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M10 20c5.523 0 10-4.477 10-10S15.523 0 10 0 0 4.477 0 10s4.477 10 10 10Z"
+      fill="#627EEA"
+    />
+    <path
+      d="M10.311 2.5v5.544l4.686 2.093L10.31 2.5Z"
+      fill="#fff"
+      fillOpacity={0.602}
+    />
+    <path d="m10.311 2.5-4.686 7.637 4.686-2.093V2.5Z" fill="#fff" />
+    <path
+      d="M10.311 13.73v3.767L15 11.01l-4.689 2.72Z"
+      fill="#fff"
+      fillOpacity={0.602}
+    />
+    <path d="M10.311 17.497v-3.768l-4.686-2.72 4.686 6.488Z" fill="#fff" />
+    <path
+      d="m10.311 12.858 4.686-2.72-4.686-2.093v4.813Z"
+      fill="#fff"
+      fillOpacity={0.2}
+    />
+    <path
+      d="m5.625 10.137 4.686 2.721V8.045l-4.686 2.092Z"
+      fill="#fff"
+      fillOpacity={0.602}
+    />
+  </svg>
+)
+
+export const inlineEthLogo = `data:image/svg+xml;base64,${btoa(
+  renderToString(<EthLogo />),
+)}`

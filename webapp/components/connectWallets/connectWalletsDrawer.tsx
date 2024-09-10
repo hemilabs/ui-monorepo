ffdae53b9407bef12b4d1ebb3b9ff767e540b754
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-09 21:11:30 -0300

    Apply new styles to connect wallets drawer

--- webapp/components/connectWallets/connectWalletsDrawer.tsx
+++ webapp/components/connectWallets/connectWalletsDrawer.tsx
@@ -11,7 +11,7 @@ import { Drawer } from 'ui-common/components/drawer'
 import { BtcWallet, EvmWallet } from './wallets'
 
 const P = ({ text }: { text: string }) => (
-  <p className="text-xs font-normal leading-normal text-slate-500">{text}</p>
+  <p className="text-ms font-medium leading-5 text-neutral-500">{text}</p>
 )
 
 type Props = {
@@ -38,20 +38,22 @@ export const ConnectWalletsDrawer = function ({ closeDrawer }: Props) {
 
   return (
     <Drawer onClose={onClose}>
-      <div className="h-full rounded-t-xl border border-solid border-slate-200 bg-white px-4 pb-12 pt-6 md:max-w-md md:rounded-xl md:p-6">
-        <div className="flex h-full flex-col gap-y-4">
+      <div className="pb-18 h-full rounded-t-xl border border-solid border-neutral-300/55 bg-white px-4 pt-6 shadow-lg md:max-w-md md:rounded-xl md:p-6 md:px-6">
+        <div className="flex h-full flex-col gap-y-3">
           <div className="flex items-center justify-between">
-            <h2 className="text-xl font-medium leading-5">
+            <h2 className="text-2xl font-medium leading-8 text-neutral-950">
               {t('common.connect-wallets')}
             </h2>
             <button className="cursor-pointer" onClick={closeDrawer}>
-              <CloseIcon />
+              <CloseIcon className="[&>path]:hover:stroke-black" />
             </button>
           </div>
           {featureFlags.btcTunnelEnabled && (
             <P text={t('connect-wallets.description')} />
           )}
-          <EvmWallet />
+          <div className="mb-3 mt-5">
+            <EvmWallet />
+          </div>
           {featureFlags.btcTunnelEnabled && (
             <>
               <BtcWallet />

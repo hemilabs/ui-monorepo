1ea2c18998b17694806bc5d250d74fe4e9018369
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-10 18:48:04 -0300

    Style tunnel tabs

--- ui-common/components/tabs.tsx
+++ ui-common/components/tabs.tsx
@@ -22,15 +22,19 @@ export const Tab = function ({
   ...props
 }: TabProps) {
   const isButton = tabIsButton(props)
-  const className = `px-5 md:px-6 py-2 text-sm font-medium inline-block h-full ${
-    selected ? 'text-orange-950' : 'text-slate-300'
-  }
-  ${disabled ? 'opacity-40' : 'opacity-100'}`
   return (
-    <li className="h-full border-y border-l border-solid border-slate-100 bg-white shadow-sm first:rounded-l-xl last:rounded-r-xl last:border-r">
+    <li
+      className={`
+      text-ms box-border flex h-7 items-center rounded-md px-2 py-1 font-medium
+      ${
+        selected
+          ? 'border border-solid border-neutral-300/55 bg-white text-neutral-950 shadow-sm'
+          : 'cursor-pointer bg-neutral-100 text-neutral-600 hover:text-neutral-950'
+      }
+    `}
+    >
       {(isButton || disabled) && (
         <button
-          className={className}
           disabled={disabled || selected}
           onClick={isButton && !disabled ? props.onClick : undefined}
           type="button"
@@ -38,11 +42,7 @@ export const Tab = function ({
           {children}
         </button>
       )}
-      {!isButton && props.href && (
-        <Link className={className} href={props.href}>
-          {children}
-        </Link>
-      )}
+      {!isButton && props.href && <Link href={props.href}>{children}</Link>}
     </li>
   )
 }
@@ -52,7 +52,5 @@ type TabsProps = {
 }
 
 export const Tabs = ({ children }: TabsProps) => (
-  <ul className="flex flex-wrap items-center border-gray-200 text-center text-sm font-medium text-gray-500">
-    {children}
-  </ul>
+  <ul className="flex flex-wrap items-center gap-x-2">{children}</ul>
 )

67fdde1b53e55c11724815d1c6c6fd6794f0074c
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-12 11:45:36 -0300

    Move some utils into their own file

---
+++ webapp/app/[locale]/tunnel/_utils/index.ts
@@ -0,0 +1,49 @@
+import Big from 'big.js'
+import { Token } from 'types/token'
+import { isNativeToken } from 'utils/token'
+import { formatUnits, parseUnits } from 'viem'
+
+import { type TunnelState } from '../_hooks/useTunnelState'
+
+type CanSubmit = Pick<
+  TunnelState,
+  'fromNetworkId' | 'fromToken' | 'fromInput'
+> & {
+  balance: bigint
+  chainId?: TunnelState['fromNetworkId']
+  fromInput: string
+}
+
+export const canSubmit = ({
+  balance,
+  chainId,
+  fromInput,
+  fromNetworkId,
+  fromToken,
+}: CanSubmit) =>
+  Big(fromInput).gt(0) &&
+  chainId === fromNetworkId &&
+  // for native tokens, it can't match the whole balance
+  // as native tokens are used to pay for fees
+  Big(fromInput)[isNativeToken(fromToken) ? 'lt' : 'lte'](
+    formatUnits(balance, fromToken.decimals),
+  )
+
+type GetTotal = {
+  fees?: bigint
+  fromInput: string
+  fromToken: Token
+}
+export const getTotal = ({
+  fees = BigInt(0),
+  fromInput,
+  fromToken,
+}: GetTotal) =>
+  formatUnits(
+    BigInt(
+      Big(parseUnits(fromInput, fromToken.decimals).toString())
+        .plus(fees.toString())
+        .toFixed(),
+    ),
+    fromToken.decimals,
+  )

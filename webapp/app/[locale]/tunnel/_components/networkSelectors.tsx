6778ed485846017026d837f8ef34a8ea588b5202
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-12 11:16:09 -0300

    Style network selectors

---
+++ webapp/app/[locale]/tunnel/_components/networkSelectors.tsx
@@ -0,0 +1,55 @@
+import { useHemi } from 'hooks/useHemi'
+import { useNetworks } from 'hooks/useNetworks'
+import { useTranslations } from 'next-intl'
+import { RemoteChain } from 'types/chain'
+
+import { type TunnelState } from '../_hooks/useTunnelState'
+
+import { NetworkSelector } from './networkSelector'
+import { ToggleButton } from './ToggleButton'
+
+type Props = {
+  isRunningOperation: boolean
+  toggleInput: () => void
+  updateFromNetwork: (network: RemoteChain['id']) => void
+  updateToNetwork: (network: RemoteChain['id']) => void
+} & Pick<TunnelState, 'fromNetworkId' | 'toNetworkId'>
+
+export const NetworkSelectors = function ({
+  fromNetworkId,
+  isRunningOperation,
+  toggleInput,
+  toNetworkId,
+  updateFromNetwork,
+  updateToNetwork,
+}: Props) {
+  const hemi = useHemi()
+  const { networks } = useNetworks()
+  const t = useTranslations('tunnel-page')
+
+  return (
+    <div className="flex items-end justify-between gap-x-3">
+      <div className="w-[calc(50%-38px-0.75rem)] flex-grow">
+        <NetworkSelector
+          disabled={isRunningOperation}
+          label={t('form.from-network')}
+          networkId={fromNetworkId}
+          networks={networks.filter(chain => chain.id !== toNetworkId)}
+          onSelectNetwork={updateFromNetwork}
+          readonly={fromNetworkId === hemi.id}
+        />
+      </div>
+      <ToggleButton disabled={isRunningOperation} toggle={toggleInput} />
+      <div className="w-[calc(50%-38px-0.75rem)] flex-grow">
+        <NetworkSelector
+          disabled={isRunningOperation}
+          label={t('form.to-network')}
+          networkId={toNetworkId}
+          networks={networks.filter(chain => chain.id !== fromNetworkId)}
+          onSelectNetwork={updateToNetwork}
+          readonly={toNetworkId === hemi.id}
+        />
+      </div>
+    </div>
+  )
+}

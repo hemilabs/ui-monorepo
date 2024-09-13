72dc43f49950e80b59cbbbcd7da67d5097370da1
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-13 16:17:15 -0300

    Style fees review and receiving address

--- webapp/app/[locale]/tunnel/_components/btcFees.tsx
+++ webapp/app/[locale]/tunnel/_components/btcFees.tsx
@@ -4,10 +4,12 @@ import { getFormattedValue } from 'utils/format'
 export const BtcFees = function ({ fees }: { fees: string }) {
   const t = useTranslations('common')
   return (
-    <div className="mt-2 flex flex-col gap-y-2 text-sm">
+    <div className="text-ms flex flex-col gap-y-1 px-8 py-4 md:px-10">
       <div className="flex items-center justify-between">
-        <span className="text-neutral-400">{t('fees')}</span>
-        <span>{`${fees ? getFormattedValue(fees) : '-'} sat/vB`}</span>
+        <span className="text-neutral-500">{t('fees')}</span>
+        <span className="text-neutral-950">{`${
+          fees ? getFormattedValue(fees) : '-'
+        } sat/vB`}</span>
       </div>
     </div>
   )
--- webapp/app/[locale]/tunnel/_components/erc20TokenApproval.tsx
+++ webapp/app/[locale]/tunnel/_components/erc20TokenApproval.tsx
@@ -1,23 +1,6 @@
+import { InfoIcon } from 'components/icons/infoIcon'
 import { useTranslations } from 'next-intl'
 import { Tooltip } from 'ui-common/components/tooltip'
-
-const InfoIcon = () => (
-  <svg fill="none" height={12} width={12} xmlns="http://www.w3.org/2000/svg">
-    <g clipPath="url(#info_icon_clip)">
-      <path
-        clipRule="evenodd"
-        d="M11.25 6A5.25 5.25 0 1 1 .75 6a5.25 5.25 0 0 1 10.5 0Zm-4.5-2.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM5.062 6a.563.563 0 1 0 0 1.125h.563v1.313a.563.563 0 1 0 1.125 0V6.562A.563.563 0 0 0 6.187 6H5.063Z"
-        fill="#A3A3A3"
-        fillRule="evenodd"
-      />
-    </g>
-    <defs>
-      <clipPath id="info_icon_clip">
-        <path d="M0 0h12v12H0z" fill="#fff" />
-      </clipPath>
-    </defs>
-  </svg>
-)
 
 type Props = {
   checked: boolean

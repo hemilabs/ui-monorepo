403aca8960222b98240997e6a6fa061576c54498
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-16 22:29:08 -0300

    Lift up topbar state in tx history

--- webapp/app/[locale]/tunnel/transaction-history/page.tsx
+++ webapp/app/[locale]/tunnel/transaction-history/page.tsx
@@ -3,7 +3,10 @@
 import { PageTitle } from 'components/pageTitle'
 import dynamic from 'next/dynamic'
 import { useTranslations } from 'next-intl'
+import { useState } from 'react'
 import Skeleton from 'react-loading-skeleton'
+
+import { type FilterOptions, TopBar } from './_components/topBar'
 
 // using CSR because useWindowSize doesn't work on SSR
 const TransactionHistory = dynamic(
@@ -12,12 +15,15 @@ const TransactionHistory = dynamic(
       mod => mod.TransactionHistory,
     ),
   {
-    loading: () => <Skeleton className="h-4/5 w-full" />,
+    loading: () => (
+      <Skeleton className="h-80 w-full rounded-2xl md:h-[500px]" />
+    ),
     ssr: false,
   },
 )
 
 const Page = function () {
+  const [filterOption, setFilterOption] = useState<FilterOptions>('all')
   const t = useTranslations('tunnel-page')
 
   return (
@@ -26,7 +32,13 @@ const Page = function () {
         subtitle={t('transaction-history.subtitle')}
         title={t('transaction-history.title')}
       />
-      <TransactionHistory />
+      <div className="rounded-2.5xl text-ms mt-6 bg-neutral-100 p-1 font-medium leading-5 md:mt-8">
+        <TopBar
+          filterOption={filterOption}
+          onFilterOptionChange={setFilterOption}
+        />
+        <TransactionHistory filterOption={filterOption} />
+      </div>
     </>
   )
 }

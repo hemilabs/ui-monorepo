b164640b3dfd4a7d128f8b2afffcb68afd618797
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-18 11:56:04 -0300

    Add arrow when hovering tx link

--- webapp/app/[locale]/tunnel/transaction-history/_components/txLink.tsx
+++ webapp/app/[locale]/tunnel/transaction-history/_components/txLink.tsx
@@ -1,5 +1,6 @@
 import { BtcTransaction } from 'btc-wallet/unisat'
 import { ExternalLink } from 'components/externalLink'
+import { ArrowDownLeftIcon } from 'components/icons/arrowDownLeftIcon'
 import { useChain } from 'hooks/useChain'
 import { type RemoteChain } from 'types/chain'
 import { type Hash } from 'viem'
@@ -13,13 +14,16 @@ export const TxLink = function ({ chainId, txHash }: Props) {
   const hash = `${txHash.slice(0, 6)}...${txHash.slice(-4)}`
   const href = `${chain?.blockExplorers?.default.url}/tx/${txHash}`
   return (
-    <ExternalLink
-      className="cursor-pointer text-neutral-600 hover:text-neutral-950"
-      href={href}
-      // needed as there's event delegation in the row
-      onClick={e => e.stopPropagation()}
-    >
-      {hash}
-    </ExternalLink>
+    <div className="group/txhash-link flex w-full items-center justify-between gap-x-2">
+      <ExternalLink
+        className="cursor-pointer text-neutral-600 hover:text-neutral-950"
+        href={href}
+        // needed as there's event delegation in the row
+        onClick={e => e.stopPropagation()}
+      >
+        {hash}
+      </ExternalLink>
+      <ArrowDownLeftIcon className="invisible group-hover/txhash-link:visible [&>path]:fill-neutral-950" />
+    </div>
   )
 }

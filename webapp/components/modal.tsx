84b952cf2b0505d23705134bd94f0f71e8afd2a5
Gonzalo D'Elia <gonzalo@bloq.com>
2024-09-20 19:11:26 -0300

    Simplify overlay injection

--- webapp/components/modal.tsx
+++ webapp/components/modal.tsx
@@ -21,14 +21,19 @@ export const Modal = function ({ children, onClose }: Props) {
   return ReactDOM.createPortal(
     <>
       <Overlay />
-      <div
-        className="pointer-event-auto fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden
-          outline-none duration-[500ms] focus:outline-none"
+      <dialog
+        className="pointer-event-auto border-neutral-300/56 fixed inset-0 z-50 flex items-center 
+          justify-center overflow-y-auto overflow-x-hidden
+          rounded-2xl border border-solid bg-white outline-none duration-[500ms] focus:outline-none"
         onTouchStart={e => e.stopPropagation()}
+        style={{
+          boxShadow:
+            '0px 2px 4px 0px rgba(0, 2, 2, 0.04), 0px 8px 24px -4px rgba(0, 2, 2, 0.04)',
+        }}
       >
         <div ref={modalRef}>{children}</div>
-      </div>
+      </dialog>
     </>,
-    document.body,
+    document.getElementById('app-layout-container'),
   )
 }

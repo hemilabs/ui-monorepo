27bcf702deea2cc03c59566c80adcb80e70ebe09
Gonzalo D'Elia <gonzalo@bloq.com>
2024-10-10 18:12:12 -0300

    Remove unused logos

--- webapp/hooks/useHemi.tsx
+++ webapp/hooks/useHemi.tsx
@@ -1,26 +1,14 @@
 import { hemiMainnet } from 'networks/hemiMainnet'
 import { hemiTestnet } from 'networks/hemiTestnet'
 import { useMemo } from 'react'
-import { renderToString } from 'react-dom/server'
 import { type EvmChain } from 'types/chain'
-import { HemiSymbolWhite } from 'ui-common/components/hemiLogo'
 
 import { useNetworkType } from './useNetworkType'
 
 export const useHemi = function () {
   const [type] = useNetworkType()
   return useMemo(
-    (): EvmChain => ({
-      ...(type === 'testnet' ? hemiTestnet : hemiMainnet),
-      // See https://github.com/hemilabs/ui-monorepo/issues/478 for when to remove
-      iconBackground: '#FFFFFF',
-      iconUrl: () =>
-        Promise.resolve(
-          `data:image/svg+xml;base64,${btoa(
-            renderToString(<HemiSymbolWhite />),
-          )}`,
-        ),
-    }),
+    (): EvmChain => (type === 'testnet' ? hemiTestnet : hemiMainnet),
     [type],
   )
 }

64e7c1f5f0b212e0a7209ae81ca0539b8a241845
Gonzalo D'Elia <gonzalo@bloq.com>
2024-10-11 13:34:55 -0300

    Fix overflow of eth chains menu

--- webapp/components/connectedWallet/evmChainsMenu.tsx
+++ webapp/components/connectedWallet/evmChainsMenu.tsx
@@ -1,5 +1,6 @@
 import { CheckMark } from 'components/icons/checkMark'
 import { Menu } from 'components/menu'
+import { useLayoutEffect, useRef, useState } from 'react'
 import { useAccount, useSwitchChain as useSwitchEvmChain } from 'wagmi'
 
 import { EvmLogo } from './evmLogo'
@@ -11,8 +12,43 @@ export const EvmChainsMenu = function ({
 }) {
   const { chainId } = useAccount()
   const { chains, switchChain } = useSwitchEvmChain()
+
+  const menuContainerRef = useRef<HTMLDivElement>(null)
+  const [position, setPosition] = useState({ bottom: 0 })
+
+  useLayoutEffect(
+    function preventMenuOverflow() {
+      const adjustPosition = function () {
+        if (!menuContainerRef.current) {
+          return
+        }
+        const rect = menuContainerRef.current.getBoundingClientRect()
+        const newPosition = { bottom: 0 }
+
+        if (rect.bottom > window.innerHeight) {
+          // Container is using h-8, which is 32px. And each chain is 32px.
+          // If we overflow the bottom container (only scenario possible, due to location of the component)
+          // we want to move "up" the size of the menu (which is N chains * 32px) + the size of the button that opens the menu (32px)
+          newPosition.bottom = 32 + chains.length * 32
+        }
+
+        setPosition(newPosition)
+      }
+
+      adjustPosition()
+      window.addEventListener('resize', adjustPosition)
+
+      return () => window.removeEventListener('resize', adjustPosition)
+    },
+    [chains.length, menuContainerRef, setPosition],
+  )
+
   return (
-    <div className="absolute bottom-0 right-0 z-20 translate-y-[calc(100%-5px)]">
+    <div
+      className="absolute bottom-0 right-0 z-20 translate-y-[calc(100%-5px)]"
+      ref={menuContainerRef}
+      style={{ bottom: position.bottom }}
+    >
       <Menu
         items={chains.map(c => ({
           content: (

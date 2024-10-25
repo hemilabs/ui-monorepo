b7e471c80fd242f48cf857b9221f2fde71336cc2
Gonzalo D'Elia <gonzalo@bloq.com>
2024-10-25 17:01:48 -0300

    Consider reconnecting status for check connection

--- webapp/hooks/useConnectedToUnsupportedChain.ts
+++ webapp/hooks/useConnectedToUnsupportedChain.ts
@@ -4,10 +4,10 @@ import { useAccount as useEvmAccount } from 'wagmi'
 export const useConnectedToUnsupportedEvmChain = function () {
   // if connected to unsupported network, "chain" is undefined
   const { chain, status } = useEvmAccount()
-  return status === 'connected' && chain === undefined
+  return ['connected', 'reconnecting'].includes(status) && chain === undefined
 }
 
 export const useConnectedToUnsupportedBtcChain = function () {
   const { chain, status } = useBtcAccount()
-  return status === 'connected' && chain === undefined
+  return ['connected', 'reconnecting'].includes(status) && chain === undefined
 }

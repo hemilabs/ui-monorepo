efd4ef7694d0d26f51389c55b225185b137a8158
Gonzalo D'elia <gonzalo@hemi.xyz>
2024-12-23 17:03:14 -0300

    Create common file for watching ops utilities

--- webapp/utils/watch/bitcoinDeposits.ts
+++ webapp/utils/watch/bitcoinDeposits.ts
@@ -1,12 +1,10 @@
 import debugConstructor from 'debug'
-import { publicClientToHemiClient } from 'hooks/useHemiClient'
-import pMemoize from 'promise-mem'
 import { type BtcDepositOperation, BtcDepositStatus } from 'types/tunnel'
 import { getTransactionReceipt } from 'utils/btcApi'
-import { findChainById } from 'utils/chain'
 import { getHemiStatusOfBtcDeposit, getVaultAddressByDeposit } from 'utils/hemi'
 import { hasKeys } from 'utils/utilities'
-import { type Chain, createPublicClient, http } from 'viem'
+
+import { getHemiClient } from './common'
 
 const debug = debugConstructor('watch-btc-deposits-worker')
 
@@ -50,16 +48,6 @@ export const watchDepositOnBitcoin = async function (
 
   return updates
 }
-
-const getHemiClient = pMemoize(async function (chainId: Chain['id']) {
-  // L2 are always EVM
-  const l2Chain = findChainById(chainId) as Chain
-  const publicClient = createPublicClient({
-    chain: l2Chain,
-    transport: http(),
-  })
-  return publicClientToHemiClient(publicClient)
-})
 
 export const watchDepositOnHemi = async function (
   deposit: BtcDepositOperation,

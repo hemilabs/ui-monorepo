5f194eb27834e59b85c3436994adc6e429220763
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-01-11 18:07:09 -0300

    Add worker watcher for bitcoin withdrawals

---
+++ webapp/context/tunnelHistoryContext/bitcoinWithdrawalsStatusUpdater.tsx
@@ -0,0 +1,129 @@
+'use client'
+
+import { WithWorker } from 'components/withWorker'
+import { useBtcWithdrawals } from 'hooks/useBtcWithdrawals'
+import { useConnectedToUnsupportedEvmChain } from 'hooks/useConnectedToUnsupportedChain'
+import { useTunnelHistory } from 'hooks/useTunnelHistory'
+import { useEffect } from 'react'
+import { BtcWithdrawStatus, ToBtcWithdrawOperation } from 'types/tunnel'
+import { isBtcWithdrawalInFinalState } from 'utils/tunnel'
+import { hasKeys } from 'utils/utilities'
+import { useAccount as useEvmAccount } from 'wagmi'
+import {
+  type AppToWorker,
+  getWithdrawalKey,
+} from 'workers/watchBitcoinWithdrawals'
+
+function WatchBitcoinWithdrawal({
+  withdrawal,
+  worker,
+}: {
+  withdrawal: ToBtcWithdrawOperation
+  worker: AppToWorker
+}) {
+  const { updateWithdrawal } = useTunnelHistory()
+
+  useEffect(
+    function watchWithdrawalUpdates() {
+      // Not using useState as 1. this value is local to the effect and
+      // 2. to avoid unsubscribing and resubscribing when the state value changes
+      let hasWorkedPostedBack = false
+
+      const saveUpdates = function (
+        event: MessageEvent<{
+          updates: Partial<ToBtcWithdrawOperation>
+          type: string
+        }>,
+      ) {
+        // This is needed because this component is rendered per-withdrawal, so each component will receive the posted message
+        // for every withdrawal, as there are many withdrawals but only one worker.
+        // Of all components, this "if" clause below will be true for only one rendered component - the one belonging to the withdrawal
+        const { type, updates } = event.data
+        if (type !== getWithdrawalKey(withdrawal)) {
+          return
+        }
+        // next interval will poll again
+        hasWorkedPostedBack = true
+        if (hasKeys(updates)) {
+          updateWithdrawal(withdrawal, updates)
+        }
+      }
+
+      worker.addEventListener('message', saveUpdates)
+
+      // refetch every 15 seconds
+      const interval = 15 * 1000
+
+      worker.postMessage({
+        type: 'watch-btc-withdrawal',
+        withdrawal,
+      })
+
+      const intervalId = setInterval(function () {
+        if (!hasWorkedPostedBack) {
+          return
+        }
+        worker.postMessage({
+          type: 'watch-btc-withdrawal',
+          withdrawal,
+        })
+        // Block posting until a response is received
+        hasWorkedPostedBack = false
+      }, interval)
+
+      return function cleanup() {
+        worker.removeEventListener('message', saveUpdates)
+        clearInterval(intervalId)
+      }
+    },
+    [updateWithdrawal, withdrawal, worker],
+  )
+
+  return null
+}
+
+// See https://github.com/vercel/next.js/issues/31009#issuecomment-11463441611
+// and https://github.com/vercel/next.js/issues/31009#issuecomment-1338645354
+const getWorker = () =>
+  new Worker(
+    new URL('../../workers/watchBitcoinWithdrawals.ts', import.meta.url),
+  )
+
+// Unless the "initiateWithdraw" tx is not confirmed, the other 2 fields should be available
+const missingInformation = (withdrawal: ToBtcWithdrawOperation) =>
+  withdrawal.status !== BtcWithdrawStatus.INITIATE_WITHDRAW_PENDING &&
+  (!withdrawal.timestamp || !withdrawal.uuid)
+
+export function BitcoinWithdrawalsStatusUpdater() {
+  const withdrawals = useBtcWithdrawals()
+  // Withdrawals  are checked against an hemi address
+  const { isConnected } = useEvmAccount()
+
+  const unsupportedChain = useConnectedToUnsupportedEvmChain()
+
+  if (!isConnected || unsupportedChain) {
+    return null
+  }
+
+  const withdrawalsToWatch = withdrawals.filter(
+    withdrawal =>
+      !isBtcWithdrawalInFinalState(withdrawal) ||
+      missingInformation(withdrawal),
+  )
+
+  return (
+    <WithWorker getWorker={getWorker}>
+      {(worker: AppToWorker) => (
+        <>
+          {withdrawalsToWatch.map(withdrawal => (
+            <WatchBitcoinWithdrawal
+              key={withdrawal.transactionHash}
+              withdrawal={withdrawal}
+              worker={worker}
+            />
+          ))}
+        </>
+      )}
+    </WithWorker>
+  )
+}
---
+++ webapp/workers/watchBitcoinWithdrawals.ts
@@ -0,0 +1,77 @@
+import { type BtcChain } from 'btc-wallet/chains'
+import debugConstructor from 'debug'
+import PQueue from 'p-queue'
+import { BtcWithdrawStatus, type ToBtcWithdrawOperation } from 'types/tunnel'
+import { isBtcWithdrawalInFinalState } from 'utils/tunnel'
+import { type EnableWorkersDebug } from 'utils/typeUtilities'
+import { watchBitcoinWithdrawal } from 'utils/watch/bitcoinWithdrawals'
+
+type WatchBtcWithdrawal = {
+  type: 'watch-btc-withdrawal'
+  withdrawal: ToBtcWithdrawOperation
+}
+
+type AppToWebWorkerActions = EnableWorkersDebug | WatchBtcWithdrawal
+
+export type AppToWorker = Omit<Worker, 'onmessage' | 'postMessage'> & {
+  postMessage: (message: AppToWebWorkerActions) => void
+}
+
+// Worker is typed with "any", so force type safety for the messages
+// (as in runtime all types are stripped, this will continue to work)
+type WatchBtcWithdrawalsWorker = Omit<Worker, 'onmessage' | 'postMessage'> & {
+  onmessage: (event: MessageEvent<AppToWebWorkerActions>) => void
+  postMessage: (event: {
+    type: `update-btc-withdrawal-${BtcChain['id']}-${ToBtcWithdrawOperation['transactionHash']}`
+    updates: Partial<ToBtcWithdrawOperation>
+  }) => void
+}
+
+// See https://github.com/Microsoft/TypeScript/issues/20595#issuecomment-587297818
+const worker = self as unknown as WatchBtcWithdrawalsWorker
+
+const hemiQueue = new PQueue({ concurrency: 3 })
+
+// See https://www.npmjs.com/package/p-queue#priority
+const getPriority = function (withdrawal: ToBtcWithdrawOperation) {
+  // Give priority to running operations
+  if (
+    [
+      BtcWithdrawStatus.CHALLENGE_IN_PROGRESS,
+      BtcWithdrawStatus.INITIATE_WITHDRAW_PENDING,
+    ].includes(withdrawal.status)
+  ) {
+    return 2
+  }
+  // if a final withdrawal reached this point, it is for missing information.
+  // Let's give them priority, so they are removed forever from the queue
+  return isBtcWithdrawalInFinalState(withdrawal) ? 1 : 0
+}
+
+export const getWithdrawalKey = (withdrawal: ToBtcWithdrawOperation) =>
+  `update-btc-withdrawal-${withdrawal.l1ChainId}-${withdrawal.transactionHash}` as const
+
+const postUpdates =
+  (withdrawal: ToBtcWithdrawOperation) =>
+  (updates: Partial<ToBtcWithdrawOperation> = {}) =>
+    worker.postMessage({
+      type: getWithdrawalKey(withdrawal),
+      updates,
+    })
+
+const watchWithdrawal = (withdrawal: ToBtcWithdrawOperation) =>
+  hemiQueue.add(
+    () => watchBitcoinWithdrawal(withdrawal).then(postUpdates(withdrawal)),
+    { priority: getPriority(withdrawal) },
+  )
+
+// wait for the UI to send chain and address once ready
+worker.onmessage = function runWorker(e: MessageEvent<AppToWebWorkerActions>) {
+  if (e.data.type === 'watch-btc-withdrawal') {
+    watchWithdrawal(e.data.withdrawal)
+  }
+  // See https://github.com/debug-js/debug/issues/916#issuecomment-1539231712
+  if (e.data.type === 'enable-debug') {
+    debugConstructor.enable(e.data.payload)
+  }
+}

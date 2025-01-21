64cb08fb6ca2d02291fc34ebf6d11df0ff2337a1
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-01-20 23:44:15 -0300

    Update fn to add rpc urls

--- webapp/networks/utils.ts
+++ webapp/networks/utils.ts
@@ -1,14 +1,18 @@
+import { isValidUrl } from 'utils/url'
 import { type Chain } from 'viem'
 import { defineChain } from 'viem/utils'
 
-export const overrideRpcUrl = function (chain: Chain, rpcUrl?: string) {
-  const isValidCustomSepoliaRpc = !!rpcUrl && rpcUrl.startsWith('https')
-  if (isValidCustomSepoliaRpc) {
+export const updateRpcUrls = function (chain: Chain, rpcUrlEnv?: string) {
+  if (typeof rpcUrlEnv !== 'string') {
+    return chain
+  }
+  const urls = rpcUrlEnv.split('+').filter(isValidUrl)
+  if (urls.length > 0) {
     return defineChain({
       ...chain,
       rpcUrls: {
         default: {
-          http: [rpcUrl],
+          http: urls,
         },
       },
     })

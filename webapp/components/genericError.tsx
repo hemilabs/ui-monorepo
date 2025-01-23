2b8eb9880474bd19ed68959f5f06fc7b63bd5c1b
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-01-23 14:48:11 -0300

    Remove old Card component

--- webapp/components/genericError.tsx
+++ webapp/components/genericError.tsx
@@ -1,11 +1,11 @@
+import { Card } from 'components/card'
 import { useTranslations } from 'next-intl'
-import { Card } from 'ui-common/components/card'
 
 export const GenericError = function () {
   const t = useTranslations()
   return (
-    <Card borderColor="gray" padding="large" radius="large">
-      <div className="flex h-[50dvh] w-full flex-col items-center justify-center gap-y-2">
+    <Card>
+      <div className="flex h-[50dvh] w-full flex-col items-center justify-center gap-y-2 p-6 md:p-9">
         <p className="text-base font-normal text-slate-500">
           {t('common.unexpected-error')}
         </p>

11b88bd0d522215d3baf63ab8adcd62fc150f490
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-02-19 11:22:14 -0300

    Skip wallet errors on Sentry

--- webapp/sentry.client.config.ts
+++ webapp/sentry.client.config.ts
@@ -2,6 +2,12 @@ import * as Sentry from '@sentry/nextjs'
 
 const enabled = !!process.env.NEXT_PUBLIC_SENTRY_DSN
 
+const unsupportedWalletErrors = [
+  'shouldSetPelagusForCurrentProvider is not a function',
+  'shouldSetTallyForCurrentProvider is not a function',
+  'Talisman extension has not been configured yet',
+]
+
 function enableSentry() {
   const ignoreErrors = [
     // Disable until WalletConnect is enabled, as it will fail for all users
@@ -9,8 +15,6 @@ function enableSentry() {
     'connection failed for host: wss://relay.walletconnect.org',
     // user rejected a confirmation in the wallet
     'rejected the request',
-    // Unsupported wallet + user error
-    'Talisman extension has not been configured yet',
     // React internal error thrown when something outside react modifies the DOM
     // This is usually because of a browser extension or Chrome's built-in translate. There's no action to do.
     // See https://blog.sentry.io/making-your-javascript-projects-less-noisy/#ignore-un-actionable-errors
@@ -18,6 +22,7 @@ function enableSentry() {
     'The node before which the new node is to be inserted is not a child of this node.',
     // Thrown when firefox prevents an add-on from referencing a DOM element that has been removed.
     `TypeError: can't access dead object`,
+    ...unsupportedWalletErrors,
   ]
 
   Sentry.init({

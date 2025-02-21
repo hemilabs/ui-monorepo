9fd0ac39528a4a913c532e184696bf3adbfe7c65
Thomas Alvarenga <thomas@hemi.xyz>
2025-02-21 13:03:07 -0300

    Added mobile footer top border

--- webapp/app/[locale]/_components/footer.tsx
+++ webapp/app/[locale]/_components/footer.tsx
@@ -1,7 +1,7 @@
 import { ConnectedChains } from 'components/connectedWallet/connectedChains'
 
 export const Footer = () => (
-  <footer className="flex h-14 w-full items-center justify-center md:hidden">
+  <footer className="flex h-14 w-full items-center justify-center border-t border-neutral-300 md:hidden">
     <ConnectedChains />
   </footer>
 )

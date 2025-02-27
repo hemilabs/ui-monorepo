245bd236699d9b6eb9479a18aaa3be03b78ff983
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-02-27 15:46:11 -0300

    Enable stable tokens in stake, and wbtc

--- webapp/app/[locale]/stake/protocols/protocolImages.ts
+++ webapp/app/[locale]/stake/protocols/protocolImages.ts
@@ -3,26 +3,36 @@ import { StakeProtocols } from 'types/stake'
 
 import bedRock from './images/bedRock.svg'
 import bitFi from './images/bitFi.svg'
+import circle from './images/circle.svg'
 import exSat from './images/exSat.svg'
 import hemi from './images/hemi.svg'
 import lorenzo from './images/lorenzo.svg'
+import makerDao from './images/makerDao.svg'
 import merlinChain from './images/merlinChain.svg'
 import obeliskNodeDao from './images/obeliskNodeDao.svg'
 import pumpBtc from './images/pumpBtc.svg'
 import solv from './images/solv.svg'
 import stakeStone from './images/stakeStone.svg'
+import tether from './images/tether.svg'
+import threshold from './images/threshold.svg'
 import uniRouter from './images/uniRouter.svg'
+import wbtc from './images/wbtc.svg'
 
 export const protocolImages: Record<StakeProtocols, StaticImageData> = {
   bedRock,
   bitFi,
+  circle,
   exSat,
   hemi,
   lorenzo,
+  makerDao,
   merlinChain,
   obeliskNodeDao,
   pumpBtc,
   solv,
   stakeStone,
+  tether,
+  threshold,
   uniRouter,
+  wbtc,
 }
--- webapp/tokenList/stakeTokens.ts
+++ webapp/tokenList/stakeTokens.ts
@@ -6,15 +6,20 @@ import { Address } from 'viem'
 const websitesMap: Partial<Record<StakeExtensions['protocol'], string>> = {
   bedRock: 'https://www.bedrock.technology',
   bitFi: 'https://www.bitfi.one',
+  circle: 'https://www.circle.com',
   exSat: 'https://exsat.network',
   hemi: 'https://www.hemi.xyz',
   lorenzo: 'https://lorenzo-protocol.xyz',
+  makerDao: 'https://makerdao.com/en',
   merlinChain: 'https://merlinchain.io',
   obeliskNodeDao: 'https://obelisk.nodedao.com/',
   pumpBtc: 'https://mainnet.pumpbtc.xyz',
   solv: 'https://solv.finance',
   stakeStone: 'https://stakestone.io/#/home',
+  tether: 'https://tether.to/en',
+  threshold: 'https://threshold.network',
   uniRouter: 'https://www.unirouter.io/#',
+  wbtc: 'https://www.wbtc.network',
 }
 
 // TODO: Some tokens are not deployed, so their rewards can't be configured
@@ -26,12 +31,11 @@ export const stakeWhiteList: Partial<
     // Prefer ordering by symbol in comments, instead of by address, which makes it harder
     // to search for a specific token.
     /* eslint-disable sort-keys */
-    // hemiBTC
-    '0xAA40c0c7644e0b2B224509571e10ad20d9C4ef28': {
-      protocol: 'hemi',
-      priceSymbol: 'btc',
+    // DAI
+    '0x6c851F501a3F24E29A8E39a29591cddf09369080': {
+      protocol: 'makerDao',
       rewards: ['hemi'],
-      website: websitesMap.hemi,
+      website: websitesMap.makerDao,
     },
     // enzoBTC
     '0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a': {
@@ -39,6 +43,13 @@ export const stakeWhiteList: Partial<
       priceSymbol: 'btc',
       rewards: ['hemi', 'lorenzo'],
       website: websitesMap.lorenzo,
+    },
+    // hemiBTC
+    '0xAA40c0c7644e0b2B224509571e10ad20d9C4ef28': {
+      protocol: 'hemi',
+      priceSymbol: 'btc',
+      rewards: ['hemi'],
+      website: websitesMap.hemi,
     },
     // iBTC
     '0x8154Aaf094c2f03Ad550B6890E1d4264B5DdaD9A': {
@@ -73,12 +84,44 @@ export const stakeWhiteList: Partial<
       rewards: ['hemi', 'lorenzo'],
       website: websitesMap.lorenzo,
     },
+    // tBTC v2
+    '0x12B6e6FC45f81cDa81d2656B974E8190e4ab8D93': {
+      protocol: 'threshold',
+      priceSymbol: 'btc',
+      rewards: ['hemi'],
+      website: websitesMap.threshold,
+    },
     // uBTC
     '0x78E26E8b953C7c78A58d69d8B9A91745C2BbB258': {
       protocol: 'uniRouter',
       priceSymbol: 'btc',
       rewards: ['hemi', 'unirouter', 'bsquared'],
       website: websitesMap.uniRouter,
+    },
+    // USDC
+    '0xad11a8BEb98bbf61dbb1aa0F6d6F2ECD87b35afA': {
+      protocol: 'circle',
+      rewards: ['hemi'],
+      website: websitesMap.circle,
+    },
+    // USDT
+    '0xbB0D083fb1be0A9f6157ec484b6C79E0A4e31C2e': {
+      protocol: 'tether',
+      rewards: ['hemi'],
+      website: websitesMap.tether,
+    },
+    // VUSD
+    '0x7A06C4AeF988e7925575C50261297a946aD204A8': {
+      protocol: 'hemi',
+      priceSymbol: 'usdt',
+      rewards: ['hemi'],
+      website: websitesMap.hemi,
+    },
+    // WBTC
+    '0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3': {
+      protocol: 'wbtc',
+      rewards: ['hemi'],
+      website: websitesMap.wbtc,
     },
     /* eslint-enable sort-keys */
   },

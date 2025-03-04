6ee2b185a932ff368905eeb17a83e231f44c4156
Thomas Alvarenga <thomas@hemi.xyz>
2025-03-04 16:14:09 -0300

    Added 404 and 500 error pages

--- webapp/app/not-found.tsx
+++ webapp/app/not-found.tsx
@@ -1,3 +1,33 @@
+'use client'
+
+import 'styles/globals.css'
+import { ExclamationMark } from 'components/icons/exclamationMark'
+import Image from 'next/image'
+
+import Error404 from './_images/404.svg'
+
 export default function NotFound() {
-  return <div>404</div>
+  return (
+    <div className="flex h-screen">
+      <Image
+        alt="404"
+        className="absolute inset-0 -top-64 m-auto w-72 md:w-fit"
+        src={Error404}
+      />
+      <div className="z-10 m-auto flex flex-col items-center gap-4">
+        <ExclamationMark />
+        <div className="text-center">
+          <h1 className="text-lg font-medium text-neutral-950">
+            Oops! Page not found
+          </h1>
+          <h3 className="mt-1 text-base font-medium text-neutral-500">
+            We can&apos;t find the page you&apos;re looking for.
+          </h3>
+        </div>
+        <a className="button button--primary" href="/">
+          Go back home
+        </a>
+      </div>
+    </div>
+  )
 }

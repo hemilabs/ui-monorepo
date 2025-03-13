762670578a7c018d559921078c825f9f3c05a8bf
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-03-13 15:14:47 -0300

    Add unisat Warning

--- webapp/components/connectWallets/connectWalletsDrawer.tsx
+++ webapp/components/connectWallets/connectWalletsDrawer.tsx
@@ -5,15 +5,21 @@ import {
 } from '@rainbow-me/rainbowkit'
 import { featureFlags } from 'app/featureFlags'
 import { Drawer, DrawerParagraph, DrawerTopSection } from 'components/drawer'
+import { ExternalLink } from 'components/externalLink'
 import { useNetworkType } from 'hooks/useNetworkType'
 import { useUmami } from 'hooks/useUmami'
 import { useTranslations } from 'next-intl'
 
 import { BtcWallet, EvmWallet } from './wallets'
+import { WarningIcon } from './warningIcon'
 
 type Props = {
   closeDrawer: () => void
 }
+
+const P = ({ children }: { children: string }) => (
+  <p className="text-center text-sm font-medium text-rose-600">{children}</p>
+)
 
 export const ConnectWalletsDrawer = function ({ closeDrawer }: Props) {
   const { accountModalOpen } = useAccountModal()
@@ -39,7 +45,7 @@ export const ConnectWalletsDrawer = function ({ closeDrawer }: Props) {
 
   return (
     <Drawer onClose={onClose}>
-      <div className="drawer-content max-md:pb-18 h-full md:max-w-md">
+      <div className="drawer-content h-full md:max-w-md">
         <div className="flex h-full flex-col gap-y-3">
           <DrawerTopSection
             heading={t('common.connect-wallets')}
@@ -58,7 +64,34 @@ export const ConnectWalletsDrawer = function ({ closeDrawer }: Props) {
           </div>
           {featureFlags.btcTunnelEnabled && (
             <>
-              <BtcWallet />
+              <div className="flex flex-col items-center gap-y-3 rounded-2xl bg-neutral-50 px-1 pb-3 pt-1">
+                <BtcWallet />
+                <div
+                  className="shadow-soft mt-1 flex size-6 items-center justify-center rounded-full border
+              border-solid border-rose-100 bg-rose-50"
+                >
+                  <WarningIcon />
+                </div>
+                <div>
+                  <P>
+                    {t(
+                      'connect-wallets.unisat-is-the-only-supported-wallet.first-line',
+                    )}
+                  </P>
+                  <P>
+                    {t(
+                      'connect-wallets.unisat-is-the-only-supported-wallet.second-line',
+                    )}
+                  </P>
+                </div>
+                <ExternalLink
+                  className="rounded-md bg-neutral-100 px-2 py-1.5 text-center
+                    text-sm font-semibold text-neutral-600 hover:text-neutral-950"
+                  href="https://docs.hemi.xyz/how-to-tutorials/using-hemi/wallet-setup/btc-wallet-setup"
+                >
+                  {t('connect-wallets.how-to-create-unisat-wallet')}
+                </ExternalLink>
+              </div>
               <DrawerParagraph>
                 {t('connect-wallets.btc-wallet-requirement')}
               </DrawerParagraph>

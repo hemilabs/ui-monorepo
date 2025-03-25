466f4f062ae4e738c8d0bc566ae6f0eea2b3e3ff
Thomas Alvarenga <thomas@hemi.xyz>
2025-03-25 11:19:17 -0300

    Removed USD price from the review operation drawer

--- webapp/components/reviewOperation/amount.tsx
+++ webapp/components/reviewOperation/amount.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { DisplayAmount } from 'components/displayAmount'
-import { FiatBalance } from 'components/fiatBalance'
 import { useTranslations } from 'next-intl'
 import { Token } from 'types/token'
 import { formatUnits } from 'viem'
@@ -16,16 +15,11 @@ export const Amount = function ({ token, value }: Props) {
   return (
     <div className="flex items-center justify-between text-sm font-medium">
       <span className="text-neutral-500">{t('total-amount')}</span>
-      <div className="flex gap-x-1">
-        <div className="text-neutral-950">
-          <DisplayAmount
-            amount={formatUnits(BigInt(value), token?.decimals ?? 18)}
-            token={token}
-          />
-        </div>
-        <div className="font-normal text-neutral-500">
-          <span>(${<FiatBalance token={token} />})</span>
-        </div>
+      <div className="text-neutral-950">
+        <DisplayAmount
+          amount={formatUnits(BigInt(value), token?.decimals ?? 18)}
+          token={token}
+        />
       </div>
     </div>
   )

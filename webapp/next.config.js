8775a2d2f684efae9ff6eca5f92f6b2b392cf359
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-03-26 16:03:21 -0300

    Add extra integrations

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -60,9 +60,13 @@ const sentryOptions = {
   authToken: process.env.SENTRY_AUTH_TOKEN,
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map,
   hideSourceMaps: true,
+  normalizeDepth: 6,
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#widen-the-upload-scope
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
   release,
   // eslint-disable-next-line camelcase
   unstable_sentryWebpackPluginOptions: {

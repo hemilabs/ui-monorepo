2c5ece9c471bb3fe210de1761ae20e666e4c0a42
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-03-31 14:33:07 -0300

    Record errors on Sentry for Prices/Points APIs

--- webapp/app/[locale]/stake/_hooks/useHemiPoints.ts
+++ webapp/app/[locale]/stake/_hooks/useHemiPoints.ts
@@ -1,5 +1,7 @@
+import * as Sentry from '@sentry/nextjs'
 import { useQuery } from '@tanstack/react-query'
 import fetch from 'fetch-plus-plus'
+import { useEffect } from 'react'
 import { useAccount } from 'wagmi'
 
 const hemiPointsUrl = process.env.NEXT_PUBLIC_POINTS_URL
@@ -7,7 +9,7 @@ const hemiPointsUrl = process.env.NEXT_PUBLIC_POINTS_URL
 export const useHemiPoints = function () {
   const { address, isConnected } = useAccount()
 
-  return useQuery({
+  const query = useQuery({
     enabled: isConnected && !!address,
     queryFn: () =>
       fetch(`${hemiPointsUrl}/${address}`).then(
@@ -15,4 +17,19 @@ export const useHemiPoints = function () {
       ) as Promise<number>,
     queryKey: ['hemi-points', address],
   })
+
+  const { error } = query
+
+  useEffect(
+    function captureHemiPriceError() {
+      if (error) {
+        Sentry.captureException(
+          new Error('Failed to fetch the Points api', { cause: error }),
+        )
+      }
+    },
+    [error],
+  )
+
+  return query
 }

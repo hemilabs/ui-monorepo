058d46d6a7d3cbe57b8c8344515db4ad58c20e95
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-04-10 13:03:12 -0300

    Use hemi-tunnel-actions to initiate withdraw

---
+++ webapp/app/[locale]/tunnel/_hooks/useEstimateWithdrawFees.ts
@@ -0,0 +1,41 @@
+import { encodeInitiateWithdraw } from 'hemi-tunnel-actions'
+import { useEstimateFees } from 'hooks/useEstimateFees'
+import { useL2BridgeAddress } from 'hooks/useL2BridgeAddress'
+import { NativeTokenSpecialAddressOnL2 } from 'tokenList/nativeTokens'
+import { EvmToken } from 'types/token'
+import { isNativeAddress } from 'utils/nativeToken'
+import { Address, Chain } from 'viem'
+import { useEstimateGas } from 'wagmi'
+
+export const useEstimateWithdrawFees = function ({
+  amount,
+  enabled = true,
+  fromToken,
+  l1ChainId,
+}: {
+  amount: bigint
+  enabled?: boolean
+  fromToken: EvmToken
+  l1ChainId: Chain['id']
+}) {
+  const l2BridgeAddress = useL2BridgeAddress(l1ChainId)
+  const isNative = isNativeAddress(fromToken.address)
+  const { data: gasUnits, isSuccess } = useEstimateGas({
+    data: encodeInitiateWithdraw({
+      amount,
+      l2TokenAddress: isNative
+        ? NativeTokenSpecialAddressOnL2
+        : (fromToken.address as Address),
+    }),
+    query: { enabled },
+    to: l2BridgeAddress,
+    value: isNative ? amount : undefined,
+  })
+
+  return useEstimateFees({
+    chainId: fromToken.chainId,
+    enabled: isSuccess,
+    gasUnits,
+    overEstimation: 1.5,
+  })
+}

c72c4c13bd098ea74f2d283c19dae97c43159a7d
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-05-08 15:35:20 -0300

    Remove unnecesary curly braces

--- portal/components/tunnelStatusUpdaters/index.tsx
+++ portal/components/tunnelStatusUpdaters/index.tsx
@@ -6,9 +6,9 @@ import { EvmWithdrawalsStateUpdater } from './evmWithdrawalsStateUpdater'
 export const TunnelStatusUpdaters = () => (
   <>
     {/* Track updates on bitcoin deposits, in bitcoin or in Hemi */}
-    {<BitcoinDepositsStatusUpdater />}
+    <BitcoinDepositsStatusUpdater />
     {/* Track updates on bitcoin withdrawals, from Hemi to Bitcoin */}
-    {<BitcoinWithdrawalsStatusUpdater />}
+    <BitcoinWithdrawalsStatusUpdater />
     {/* Track updates on evm withdrawals from Hemi */}
     <EvmWithdrawalsStateUpdater />
     {/* Track updates on deposits to Hemi, tracking any missing info */}

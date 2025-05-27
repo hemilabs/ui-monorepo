b24fa8d4d7bc086cf01a5d39f41376a673370c71
Thomas Alvarenga <thomas@hemi.xyz>
2025-05-27 17:05:56 -0300

    Removed TVL card when network is testnet

--- portal/app/[locale]/_components/navbar/_components/tvl/index.tsx
+++ portal/app/[locale]/_components/navbar/_components/tvl/index.tsx
@@ -1,3 +1,4 @@
+import { useNetworkType } from 'hooks/useNetworkType'
 import { useTvl } from 'hooks/useTvl'
 import { useTranslations } from 'next-intl'
 import React from 'react'
@@ -10,46 +11,50 @@ import { HemiLogo } from './hemiLogo'
 export const Tvl = function () {
   const { data, isError, isPending } = useTvl()
   const t = useTranslations('navbar')
+  const [networkType] = useNetworkType()
+  const isNotTestnet = networkType !== 'testnet'
 
   const getAmount = function () {
     if (isError) {
       return '-'
     }
 
-    return new Intl.NumberFormat('en', {
+    return `$${new Intl.NumberFormat('en', {
       compactDisplay: 'short',
       maximumFractionDigits: 2,
       notation: 'compact',
-    }).format(data)
+    }).format(data)}`
   }
 
   return (
-    <section
-      className="shadow-soft h-22 relative mb-4 mt-4 w-full overflow-hidden rounded-lg
+    isNotTestnet && (
+      <section
+        className="shadow-soft h-22 relative mb-4 mt-4 w-full overflow-hidden rounded-lg
       md:mb-0"
-      style={{
-        background:
-          'linear-gradient(0deg, #262626, #262626),linear-gradient(0deg, #FFFFFF, #FFFFFF),linear-gradient(180deg, rgba(0, 0, 0, 0) 10.65%, rgba(0, 0, 0, 0.6) 76.58%)',
-      }}
-    >
-      <HemiLogo className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform" />
-      <div className="absolute left-0 top-0 w-full">
-        <div className="flex flex-col gap-y-2 p-4">
-          <div className="flex w-full items-center justify-between">
-            <span className="font-inter-variable text-sm font-medium text-white">
-              {t('tvl')}:
-            </span>
-            <DollarSign />
-          </div>
-          <div className="-translate-x-1">
-            {isPending ? (
-              <Skeleton baseColor="#262626" highlightColor="#303030" />
-            ) : (
-              <Amount value={`$${getAmount()}`} />
-            )}
+        style={{
+          background:
+            'linear-gradient(0deg, #262626, #262626),linear-gradient(0deg, #FFFFFF, #FFFFFF),linear-gradient(180deg, rgba(0, 0, 0, 0) 10.65%, rgba(0, 0, 0, 0.6) 76.58%)',
+        }}
+      >
+        <HemiLogo className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform" />
+        <div className="absolute left-0 top-0 w-full">
+          <div className="flex flex-col gap-y-2 p-4">
+            <div className="flex w-full items-center justify-between">
+              <span className="font-inter-variable text-sm font-medium text-white">
+                {t('tvl')}:
+              </span>
+              <DollarSign />
+            </div>
+            <div className="-translate-x-1">
+              {isPending ? (
+                <Skeleton baseColor="#262626" highlightColor="#303030" />
+              ) : (
+                <Amount value={`${getAmount()}`} />
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    )
   )
 }

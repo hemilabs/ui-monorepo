e83ce5ca19d6f68e39e6ffabd61bb5c353425855
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-06-16 13:01:37 -0300

    Allow get-started to render statically

--- portal/app/[locale]/_components/navbar/_components/getStarted/index.tsx
+++ portal/app/[locale]/_components/navbar/_components/getStarted/index.tsx
@@ -5,69 +5,89 @@ import { useUmami } from 'hooks/useUmami'
 import { usePathname } from 'i18n/navigation'
 import Image from 'next/image'
 import { useTranslations } from 'next-intl'
-import React from 'react'
+import React, { ComponentProps, Suspense } from 'react'
 
 import hemiLogo from './_images/hemiLogo.png'
 import { Background } from './background'
 
-export const GetStarted = function () {
-  const [networkType] = useNetworkType()
-  const pathname = usePathname()
-  const t = useTranslations('navbar')
-  const { track } = useUmami()
+type Props = Pick<ComponentProps<typeof Link>, 'href' | 'onClick'> & {
+  active: boolean
+  t: ReturnType<typeof useTranslations<'navbar'>>
+}
 
-  const href = '/get-started'
-  const onClick = () => track?.('nav - get started', { chain: networkType })
-  const active = pathname.endsWith('/get-started/')
-
-  return (
-    <>
-      <Link
-        className={`shadow-soft font-inter-variable mx-4 mb-4 flex
+const UI = ({ active, href, onClick, t }: Props) => (
+  <>
+    <Link
+      className={`shadow-soft font-inter-variable mx-4 mb-4 flex
         cursor-pointer items-center justify-center gap-x-1 rounded-lg border
         border-neutral-300/55 bg-white py-2 text-sm font-semibold text-neutral-950 md:hidden
         `}
-        href={href}
-        onClick={track ? onClick : undefined}
-      >
-        <span>{t('get-started')}</span>
-        <Chevron.Right className="[&>path]:fill-neutral-500" />
-      </Link>
-      <Link
-        className={`hidden ${
-          active ? '' : 'cursor-pointer'
-        } group/item md:block`}
-        href={href}
-        onClick={track ? onClick : undefined}
-      >
-        <div className="shadow-soft relative">
-          <Background
-            className={`rounded-lg border
+      href={href}
+      onClick={onClick}
+    >
+      <span>{t('get-started')}</span>
+      <Chevron.Right className="[&>path]:fill-neutral-500" />
+    </Link>
+    <Link
+      className={`hidden ${active ? '' : 'cursor-pointer'} group/item md:block`}
+      href={href}
+      onClick={onClick}
+    >
+      <div className="shadow-soft relative">
+        <Background
+          className={`rounded-lg border
             ${
               active
                 ? 'border-orange-500 [&>g>rect]:fill-orange-50'
                 : 'border-neutral-300/55'
             }
             group-hover/item:[&>g>rect]:fill-orange-50`}
-          />
-          <div className="absolute left-0 top-0 flex h-full w-full flex-col justify-between">
-            <div className="m-auto mt-7">
-              <Image alt={'hemi-logo'} className="size-13" src={hemiLogo} />
-            </div>
-            <div className="flex flex-col justify-center px-4 pb-4">
-              <div className="flex items-center">
-                <span className="text-base text-neutral-950">
-                  {t('get-started')}
-                </span>
-                <Chevron.Right className="pl-1 [&>path]:fill-neutral-500" />
-              </div>
-              <span className="text-sm text-neutral-500">
-                {t('learn-how-to-start')}
+        />
+        <div className="absolute left-0 top-0 flex h-full w-full flex-col justify-between">
+          <div className="m-auto mt-7">
+            <Image alt={'hemi-logo'} className="size-13" src={hemiLogo} />
+          </div>
+          <div className="flex flex-col justify-center px-4 pb-4">
+            <div className="flex items-center">
+              <span className="text-base text-neutral-950">
+                {t('get-started')}
               </span>
+              <Chevron.Right className="pl-1 [&>path]:fill-neutral-500" />
             </div>
+            <span className="text-sm text-neutral-500">
+              {t('learn-how-to-start')}
+            </span>
           </div>
         </div>
-      </Link>
-    </>
+      </div>
+    </Link>
+  </>
+)
+
+const GetStartedImpl = function (props: Omit<Props, 'onClick'>) {
+  const [networkType] = useNetworkType()
+
+  const { track } = useUmami()
+
+  const onClick = () => track?.('nav - get started', { chain: networkType })
+
+  return <UI {...props} onClick={onClick} />
+}
+
+export const GetStarted = function () {
+  const pathname = usePathname()
+
+  const props = {
+    active: pathname.endsWith('/get-started/'),
+    href: '/get-started',
+    t: useTranslations('navbar'),
+  }
+
+  // The UI for the get started is the same for mainnet|testnet
+  // the only change is how we track on analytics.
+  return (
+    <Suspense fallback={<UI {...props} />}>
+      <GetStartedImpl {...props} />
+    </Suspense>
   )
 }

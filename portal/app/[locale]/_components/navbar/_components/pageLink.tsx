30d7d3c7b57682e9dd547f083d9a3a649293b0d0
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-06-16 12:02:41 -0300

    Update pageLink to be statically rendered

--- portal/app/[locale]/_components/navbar/_components/pageLink.tsx
+++ portal/app/[locale]/_components/navbar/_components/pageLink.tsx
@@ -2,6 +2,7 @@ import { useUmami } from 'app/analyticsEvents'
 import { Link } from 'components/link'
 import { useNetworkType } from 'hooks/useNetworkType'
 import { usePathnameWithoutLocale } from 'hooks/usePathnameWithoutLocale'
+import { Suspense } from 'react'
 
 import {
   IconContainer,
@@ -11,6 +12,49 @@ import {
   Row,
 } from './navItem'
 
+type Props = {
+  selected?: boolean
+  onClick?: () => void
+} & ItemLinkProps
+
+const PageLinkUI = ({
+  href,
+  icon,
+  onClick,
+  rightSection,
+  selected,
+  text,
+}: Omit<Props, 'event' | 'urlToBeSelected'>) => (
+  <ItemContainer onClick={onClick} selected={selected}>
+    <Link href={href}>
+      <Row>
+        <IconContainer selected={selected}>{icon}</IconContainer>
+        <ItemText selected={selected} text={text} />
+        {rightSection}
+      </Row>
+    </Link>
+  </ItemContainer>
+)
+
+const PageLinkImpl = function ({
+  event,
+  ...props
+}: Omit<Props, 'urlToBeSelected'>) {
+  const [networkType] = useNetworkType()
+  const { track } = useUmami()
+
+  return (
+    <PageLinkUI
+      {...props}
+      onClick={
+        track && !!event
+          ? () => track(event, { chain: networkType })
+          : undefined
+      }
+    />
+  )
+}
+
 export const PageLink = function ({
   event,
   href,
@@ -19,31 +63,24 @@ export const PageLink = function ({
   text,
   urlToBeSelected = href,
 }: ItemLinkProps) {
-  const [networkType] = useNetworkType()
   const pathname = usePathnameWithoutLocale()
-  const { track } = useUmami()
 
   const selected =
     typeof urlToBeSelected === 'string'
       ? pathname.startsWith(urlToBeSelected)
       : pathname.startsWith(urlToBeSelected.pathname)
 
+  const props = {
+    href,
+    icon,
+    rightSection,
+    selected,
+    text,
+  }
+
   return (
-    <ItemContainer
-      onClick={
-        track && !!event
-          ? () => track(event, { chain: networkType })
-          : undefined
-      }
-      selected={selected}
-    >
-      <Link href={href}>
-        <Row>
-          <IconContainer selected={selected}>{icon}</IconContainer>
-          <ItemText selected={selected} text={text} />
-          {rightSection}
-        </Row>
-      </Link>
-    </ItemContainer>
+    <Suspense fallback={<PageLinkUI {...props} />}>
+      <PageLinkImpl event={event} {...props} />
+    </Suspense>
   )
 }

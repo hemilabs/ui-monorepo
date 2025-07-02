a9f8f2b646f6afb47399cd45179c5e881a87f52c
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-07-02 14:18:16 -0300

    Remove custom slate conf and use neutral grey

--- portal/components/genericError.tsx
+++ portal/components/genericError.tsx
@@ -6,7 +6,7 @@ export const GenericError = function () {
   return (
     <Card>
       <div className="flex h-[50dvh] w-full flex-col items-center justify-center gap-y-2 p-6 md:p-9">
-        <p className="text-base font-normal text-slate-500">
+        <p className="text-base font-normal text-neutral-500">
           {t('common.unexpected-error')}
         </p>
         <div className="mt-2"></div>

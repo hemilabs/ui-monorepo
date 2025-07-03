b6c81dc4d7286967c4b5c51a2259976921a1c65e
Gabriel Montes <gabriel@hemi.xyz>
2025-07-03 16:14:06 -0400

    Use token prices from backend API

--- portal/hooks/useTokenPrices.ts
+++ portal/hooks/useTokenPrices.ts
@@ -2,7 +2,7 @@ import { useQuery, UseQueryOptions } from '@tanstack/react-query'
 import fetch from 'fetch-plus-plus'
 import { isValidUrl } from 'utils/url'
 
-const pricesUrl = process.env.NEXT_PUBLIC_TOKEN_PRICES_URL
+const portalApiUrl = process.env.NEXT_PUBLIC_PORTAL_API_URL
 
 type Prices = Record<string, string>
 
@@ -12,9 +12,11 @@ export const useTokenPrices = (
   useQuery({
     // If the URL is not set, prices are not returned. Consumers of the hook
     // should consider this scenario
-    enabled: pricesUrl !== undefined && isValidUrl(pricesUrl),
+    enabled: portalApiUrl !== undefined && isValidUrl(portalApiUrl),
     queryFn: () =>
-      fetch(pricesUrl).then(({ prices }) => prices) as Promise<Prices>,
+      fetch(`${portalApiUrl}/prices`).then(
+        ({ prices }) => prices,
+      ) as Promise<Prices>,
     queryKey: ['token-prices'],
     // refetch every 5 min
     refetchInterval: 5 * 60 * 1000,

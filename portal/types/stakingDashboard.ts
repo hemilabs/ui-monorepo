b8b6923ab15787a12df3388f96ceb7f6940365c5
arturdolzan <arturdolzan@gmail.com>
2025-08-06 12:01:39 +0200

    Removed unused type

--- portal/types/stakingDashboard.ts
+++ portal/types/stakingDashboard.ts
@@ -1,14 +1,9 @@
 import { Chain, Hash } from 'viem'
 
-import { type EvmToken, type Extensions } from './token'
-
 /**
  * // TODO - This types are not fully defined yet.
  *
  */
-export type StakingDashboardToken = EvmToken & {
-  extensions: Omit<Extensions, 'protocol'>
-}
 
 export type StakingDashboardOperation = {
   amount: string

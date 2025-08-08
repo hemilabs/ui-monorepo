6c437ff5d2f744c0ecb8ebb21f97597cf27be2bb
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-08-08 16:25:03 -0300

    Add drawer for claiming progress

--- portal/app/[locale]/claim/_components/claimOptions.tsx
+++ portal/app/[locale]/claim/_components/claimOptions.tsx
@@ -1,3 +1,4 @@
+import { MutationStatus } from '@tanstack/react-query'
 import { Button } from 'components/button'
 import { Spinner } from 'components/spinner'
 import dynamic from 'next/dynamic'
@@ -6,46 +7,95 @@ import { useState } from 'react'
 import { LockupMonths, lockupOptions, type EligibilityData } from 'tge-claim'
 import { Hash } from 'viem'
 
+import { useClaimTokens } from '../_hooks/useClaimTokens'
+
+import { Strategy } from './strategy'
+
 const TermsAndConditions = dynamic(() =>
   import('./termsAndConditions').then(mod => mod.TermsAndConditions),
 )
 
-import { useClaimTokens } from '../_hooks/useClaimTokens'
-
-import { Strategy } from './strategy'
+const ClaimDrawer = dynamic(() =>
+  import('./claimDrawer').then(mod => mod.ClaimDrawer),
+)
 
 type Props = {
   eligibility: EligibilityData
 }
 
+// Currently, we don't allow customization for users of the ratio
+// so we default to 50%
+const defaultRatio = 50
+
 export const ClaimOptions = function ({ eligibility }: Props) {
-  const { mutate: claimTokens, status } = useClaimTokens()
   const [termsAndConditions, setTermsAndConditions] = useState<{
     lockup: LockupMonths | undefined
     show: boolean
   }>({ lockup: undefined, show: false })
+
+  const [isRetrying, setIsRetrying] = useState(false)
+  const [signedTerms, setSignedTerms] = useState<Hash | undefined>(undefined)
+  const [transactionHash, setTransactionHash] = useState<Hash | undefined>(
+    undefined,
+  )
+  const [claimStatus, setClaimStatus] = useState<MutationStatus>('idle')
   const t = useTranslations('rewards-page')
 
-  const onSubmit = function (lockupMonths: LockupMonths) {
-    setTermsAndConditions({ lockup: lockupMonths, show: true })
+  const closeDrawer = function () {
+    setTermsAndConditions({ lockup: undefined, show: false })
+    setClaimStatus('idle')
   }
+
+  const { mutate: claimTokens } = useClaimTokens({
+    on(emitter) {
+      emitter.on('user-signing-claim-error', () => closeDrawer())
+      emitter.on('user-signed-claim', function (hash) {
+        // no need to sign T&C again - we can reuse the signature, and retry from the drawer
+        setTermsAndConditions(prev => ({ ...prev, show: false }))
+        setTransactionHash(hash)
+        setClaimStatus('pending')
+        setIsRetrying(false)
+      })
+      emitter.on('claim-transaction-succeeded', function () {
+        setTermsAndConditions(prev => ({ ...prev, lockup: undefined }))
+        setClaimStatus('success')
+        closeDrawer()
+        setTransactionHash(undefined)
+      })
+      emitter.on('claim-transaction-reverted', () => setClaimStatus('error'))
+    },
+  })
+
+  const onSubmit = (lockupMonths: LockupMonths) =>
+    setTermsAndConditions({ lockup: lockupMonths, show: true })
 
   const disableSubmit =
-    termsAndConditions.show || ['pending', 'success'].includes(status)
+    termsAndConditions.show || ['pending', 'success'].includes(claimStatus)
 
-  const handleAcceptTermsAndConditions = function (termsSignature: Hash) {
-    setTermsAndConditions(prev => ({ ...prev, show: false }))
+  const handleClaim = (termsSignature: Hash) =>
     claimTokens({
       lockupMonths: termsAndConditions.lockup!,
-      // Currently, we don't allow customization for users of the ratio
-      // so we default to 50%
-      ratio: 50,
+
+      ratio: defaultRatio,
       termsSignature,
     })
+
+  const handleRetry = function () {
+    handleClaim(signedTerms)
+    setIsRetrying(true)
   }
 
+  const handleAcceptTermsAndConditions = function (termsSignature: Hash) {
+    // save it for retrying
+    setSignedTerms(termsSignature)
+    handleClaim(termsSignature)
+  }
+
+  const showDrawer =
+    claimStatus !== 'idle' && termsAndConditions.lockup !== undefined
+
   const getSubmitButtonText = function (strategyLockupMonths: LockupMonths) {
-    if (termsAndConditions.lockup === strategyLockupMonths) {
+    if (termsAndConditions.lockup === strategyLockupMonths && !showDrawer) {
       return (
         <div className="flex items-center gap-x-1.5">
           <Spinner size="xSmall" />
@@ -112,6 +162,19 @@ export const ClaimOptions = function ({ eligibility }: Props) {
           onClose={() =>
             setTermsAndConditions({ lockup: undefined, show: false })
           }
+        />
+      )}
+      {showDrawer && (
+        <ClaimDrawer
+          eligibility={eligibility}
+          isRetrying={isRetrying}
+          lockupMonths={termsAndConditions.lockup}
+          onClose={closeDrawer}
+          onRetry={handleRetry}
+          ratio={defaultRatio}
+          status={claimStatus}
+          termsSignature={signedTerms}
+          transactionHash={transactionHash}
         />
       )}
     </>
--- portal/app/[locale]/claim/_hooks/useClaimTokens.ts
+++ portal/app/[locale]/claim/_hooks/useClaimTokens.ts
@@ -1,8 +1,11 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { type EventEmitter } from 'events'
+import { useNativeTokenBalance } from 'hooks/useBalance'
 import { useHemi } from 'hooks/useHemi'
 import { useHemiWalletClient } from 'hooks/useHemiClient'
+import { useUpdateNativeBalanceAfterReceipt } from 'hooks/useInvalidateNativeBalanceAfterReceipt'
 import { useIsConnectedToExpectedNetwork } from 'hooks/useIsConnectedToExpectedNetwork'
-import { LockupMonths } from 'tge-claim'
+import { ClaimEvents, LockupMonths } from 'tge-claim'
 import { claimTokens } from 'tge-claim/actions'
 import { Hash } from 'viem'
 import { useAccount, useSwitchChain } from 'wagmi'
@@ -11,7 +14,9 @@ import { useEligibleForTokens } from './useEligibleForTokens'
 import { getClaimTransactionQueryKey } from './useGetClaimTransaction'
 import { getIsClaimableQueryKey } from './useIsClaimable'
 
-export const useClaimTokens = function () {
+export const useClaimTokens = function (options?: {
+  on(emitter: EventEmitter<ClaimEvents>): void
+}) {
   const { address } = useAccount()
   const eligibility = useEligibleForTokens()
   const hemi = useHemi()
@@ -19,6 +24,14 @@ export const useClaimTokens = function () {
   const { hemiWalletClient } = useHemiWalletClient()
   const queryClient = useQueryClient()
   const { switchChainAsync } = useSwitchChain()
+
+  const { queryKey: nativeTokenBalanceQueryKey } = useNativeTokenBalance(
+    hemi.id,
+  )
+
+  const updateNativeBalanceAfterFees = useUpdateNativeBalanceAfterReceipt(
+    hemi.id,
+  )
 
   const claimTransactionQueryKey = getClaimTransactionQueryKey({
     address,
@@ -56,23 +69,36 @@ export const useClaimTokens = function () {
       })
 
       emitter.on('claim-transaction-succeeded', function (receipt) {
-        // optimistically update the claim transaction
-        // and set isClaimable to false. Then, onSettled, revalidate both.
+        // update native balance on the wallet due to gas paid
+        updateNativeBalanceAfterFees(receipt)
         queryClient.setQueryData(claimTransactionQueryKey, {
           amount: eligibility.amount,
           lockupMonths,
           ratio,
           transactionHash: receipt.transactionHash,
         })
-
+        // optimistically update isClaimable to false.
         queryClient.setQueryData(isClaimableKey, false)
       })
+
+      emitter.on('claim-transaction-reverted', function (receipt) {
+        // update native balance on the wallet due to gas paid
+        updateNativeBalanceAfterFees(receipt)
+      })
+
+      options?.on(emitter)
 
       return promise
     },
     onSettled() {
-      queryClient.invalidateQueries({ queryKey: claimTransactionQueryKey })
+      // invalidate gas balance on the wallet
+      queryClient.invalidateQueries({ queryKey: nativeTokenBalanceQueryKey })
+      // invalidate updated contracts
       queryClient.invalidateQueries({ queryKey: isClaimableKey })
+      // Do not invalidate claimTransactionQueryKey - as this is not reading the contract
+      // but a subgraph through an API, there may be a small delay, and the API may not return the found transaction
+      // let's use the optimistic update instead to show the success page
+      // from what I could test, it may take at most only a few seconds for the TX to show up, but it is not instant
     },
   })
 }

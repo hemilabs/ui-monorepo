80b4bb4c1e9e9217aa4d8435a281a87e2ff5215a
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-08-22 17:47:40 -0300

    Add claim-checker background animation

--- portal/app/[locale]/genesis-drop/_components/background.tsx
+++ portal/app/[locale]/genesis-drop/_components/background.tsx
@@ -8,6 +8,20 @@ const LeftBackground = (props: ComponentProps<'svg'>) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
+    <defs>
+      <path
+        d="m-307.317-103.999 375.43 377.146c70.284 70.605 70.023 184.816-.582 255.101L-33.197 634.503"
+        id="leftPath1"
+      />
+      <path
+        d="m-246.159 971.012 392.421-392.418c62.852-62.851 62.852-164.754 0-227.605L-221.03-16.301"
+        id="leftPath2"
+      />
+      <path
+        d="m-204.116 971.613 372.003-372c74.457-74.461 74.457-195.184 0-269.645L-178.386-16.301"
+        id="leftPath3"
+      />
+    </defs>
     <path
       d="m-307.317-103.999 375.43 377.146c70.284 70.605 70.023 184.816-.582 255.101L-33.197 634.503"
       stroke="#E9F8F9"
@@ -26,10 +40,23 @@ const LeftBackground = (props: ComponentProps<'svg'>) => (
       strokeLinecap="round"
       strokeWidth={22}
     />
-    <path
-      d="M58.782 211.619a8.468 8.468 0 0 1-8.47 8.469 8.466 8.466 0 0 1-8.468-8.469 8.469 8.469 0 1 1 16.938 0ZM34.779 231.959a8.468 8.468 0 0 1-8.47 8.469 8.466 8.466 0 0 1-8.468-8.469 8.469 8.469 0 1 1 16.938 0Z"
-      fill="#fff"
-    />
+
+    {/* Animated circles following the paths */}
+    <circle fill="#fff" r="8.469">
+      <animateMotion dur="8s" repeatCount="indefinite">
+        <mpath href="#leftPath1" />
+      </animateMotion>
+    </circle>
+    <circle fill="#fff" r="8.469">
+      <animateMotion begin="1s" dur="6s" repeatCount="indefinite">
+        <mpath href="#leftPath2" />
+      </animateMotion>
+    </circle>
+    <circle fill="#fff" r="8.469">
+      <animateMotion begin="2s" dur="7s" repeatCount="indefinite">
+        <mpath href="#leftPath3" />
+      </animateMotion>
+    </circle>
   </svg>
 )
 const RightBackground = (props: ComponentProps<'svg'>) => (
@@ -40,6 +67,20 @@ const RightBackground = (props: ComponentProps<'svg'>) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
+    <defs>
+      <path
+        d="M542.046-103.999 166.617 273.147c-70.284 70.605-70.024 184.816.582 255.101l100.727 106.255"
+        id="rightPath1"
+      />
+      <path
+        d="M480.889 971.011 88.468 578.593c-62.852-62.851-62.852-164.754 0-227.605L455.76-16.302"
+        id="rightPath2"
+      />
+      <path
+        d="m438.846 971.612-372.003-372c-74.457-74.461-74.457-195.184 0-269.645L413.116-16.302"
+        id="rightPath3"
+      />
+    </defs>
     <path
       d="M542.046-103.999 166.617 273.147c-70.284 70.605-70.024 184.816.582 255.101l100.727 106.255"
       stroke="#E9F8F9"
@@ -58,10 +99,23 @@ const RightBackground = (props: ComponentProps<'svg'>) => (
       strokeLinecap="round"
       strokeWidth={22}
     />
-    <path
-      d="M175.948 211.619a8.468 8.468 0 0 0 8.469 8.469 8.466 8.466 0 0 0 8.469-8.469 8.469 8.469 0 1 0-16.938 0ZM199.951 231.959a8.468 8.468 0 0 0 8.469 8.469 8.466 8.466 0 0 0 8.469-8.469 8.469 8.469 0 1 0-16.938 0Z"
-      fill="#fff"
-    />
+
+    {/* Animated circles following the paths */}
+    <circle fill="#fff" r="8.469">
+      <animateMotion begin="0.5s" dur="8s" repeatCount="indefinite">
+        <mpath href="#rightPath1" />
+      </animateMotion>
+    </circle>
+    <circle fill="#fff" r="8.469">
+      <animateMotion begin="1.5s" dur="6s" repeatCount="indefinite">
+        <mpath href="#rightPath2" />
+      </animateMotion>
+    </circle>
+    <circle fill="#fff" r="8.469">
+      <animateMotion begin="2.5s" dur="7s" repeatCount="indefinite">
+        <mpath href="#rightPath3" />
+      </animateMotion>
+    </circle>
   </svg>
 )
 

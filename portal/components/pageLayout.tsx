dab2e9bebada08da1eab968b318a174418be7ab7
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-08-27 15:14:40 -0300

    Re-add mx-auto to pageLayout.tsx

--- portal/components/pageLayout.tsx
+++ portal/components/pageLayout.tsx
@@ -2,10 +2,10 @@ import { ReactNode } from 'react'
 
 /* eslint-disable sort-keys */
 const variants = {
-  center: 'mx-auto max-w-5xl px-4',
+  center: 'max-w-5xl px-4',
   genesisDrop: 'px-2 md:px-4 xl:px-0 xl:pb-6 2xl:px-6',
-  wide: 'mx-auto xl:px-12 xl:pb-12 px-4',
-  superWide: 'mx-auto px-2 md:px-4 xl:px-6 xl:pb-6',
+  wide: 'xl:px-12 xl:pb-12 px-4',
+  superWide: 'px-2 md:px-4 xl:px-6 xl:pb-6',
 } as const
 /* eslint-enable sort-keys */
 
@@ -15,5 +15,5 @@ type Props = {
 }
 
 export const PageLayout = ({ children, variant }: Props) => (
-  <div className={variants[variant]}>{children}</div>
+  <div className={`mx-auto ${variants[variant]}`}>{children}</div>
 )

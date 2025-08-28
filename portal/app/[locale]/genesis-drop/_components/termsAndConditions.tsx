89b8864c342e2265422b0d8ce9eb1603d77f9ddf
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-08-28 15:26:02 -0300

    Update Terms and Conditions legal text

--- portal/app/[locale]/genesis-drop/_components/termsAndConditions.tsx
+++ portal/app/[locale]/genesis-drop/_components/termsAndConditions.tsx
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query'
 import { Button } from 'components/button'
+import { ExternalLink } from 'components/externalLink'
 import { Modal } from 'components/modal'
 import { useHemi } from 'hooks/useHemi'
 import { useHemiWalletClient } from 'hooks/useHemiClient'
@@ -9,8 +10,37 @@ import { FormEvent } from 'react'
 import { Hash } from 'viem'
 import { useAccount, useSwitchChain } from 'wagmi'
 
-// TODO This message needs to be defined by legal
-const SigningMessage = 'Dummy message to sign'
+// This text is a legal one so we can't translate it
+const termsAndConditions = `By clicking "Accept" below, you agree to the {termsAndConditions} of all Hemi websites and represent and warrant that (i) you are not a citizen of, resident in, or formed or qualified to transact business in, the United States or the Peoples Republic of China, (ii) in connection with claiming any Hemi Token you are in compliance with all applicable laws of the jurisdiction in which you reside or are located and of the United States and the Peoples Republic of China, and (iii) neither you nor, to your knowledge, any of your affiliates or direct beneficial owners, (A) appears on any governmental sanctions or similar list,  nor are they otherwise a party with which the Hemi is prohibited to deal under the laws of the United States, (B) is a person identified as a terrorist organization on any other relevant lists maintained by governmental authorities, nor is a senior foreign political figure, or any immediate family member or close associate of a senior foreign political figure, and (C) by claiming Hemi tokens will not be in violation of applicable U.S. federal or state or non-U.S. laws or regulations, including, without limitation, anti-money laundering, economic sanctions, anti-bribery or anti-boycott laws or regulations.`
+
+// The signing message needs to be a plain string...
+const SigningMessage = termsAndConditions.replace(
+  '{termsAndConditions}',
+  'Terms & Conditions (https://hemi.xyz/terms-of-service)',
+)
+// but the UI needs to display the link to the T&C
+const LegalText = function () {
+  const [before, after] = termsAndConditions.split('{termsAndConditions}')
+
+  return (
+    <p
+      className="h-80 overflow-y-auto p-6 text-sm font-normal text-neutral-500"
+      style={{
+        scrollbarColor: '#d4d4d4 transparent',
+        scrollbarWidth: 'thin',
+      }}
+    >
+      {before}
+      <ExternalLink
+        className="text-orange-500 hover:text-orange-700"
+        href="https://hemi.xyz/terms-of-service"
+      >
+        Terms & Conditions
+      </ExternalLink>
+      {after}
+    </p>
+  )
+}
 
 type Props = {
   onAccept: (signedTermsAndConditions: Hash) => void
@@ -58,9 +88,7 @@ export const TermsAndConditions = function ({ onAccept, onClose }: Props) {
             {t('genesis-drop.terms-and-conditions.title')}
           </h3>
           <div className="h-px w-full border-b border-solid border-b-neutral-300/55" />
-          <p className="h-80 p-6 text-sm font-normal text-neutral-500">
-            {t('genesis-drop.terms-and-conditions.terms')}
-          </p>
+          <LegalText />
         </div>
         <form
           className="h-15 relative flex w-full items-center justify-between gap-x-4 rounded-b-xl bg-neutral-50 px-4 [&>button]:w-full"

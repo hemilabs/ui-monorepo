d083219f04216cd5bd6b0f2b0b48f57427468e18
arturdolzan <arturdolzan@gmail.com>
2025-09-11 09:07:22 -0400

    Adding files

---
+++ portal/app/[locale]/staking-dashboard/_components/stakeReview/reviewUnlock.tsx
@@ -0,0 +1,109 @@
+'use client'
+
+import { ChainLabel } from 'components/reviewOperation/chainLabel'
+import { Operation } from 'components/reviewOperation/operation'
+import {
+  ProgressStatus,
+  ProgressStatusType,
+} from 'components/reviewOperation/progressStatus'
+import { type StepPropsWithoutPosition } from 'components/reviewOperation/step'
+import { useHemi } from 'hooks/useHemi'
+import { useHemiToken } from 'hooks/useHemiToken'
+import { useTranslations } from 'next-intl'
+import { UnlockingDashboardStatus } from 'types/stakingDashboard'
+import { getNativeToken } from 'utils/nativeToken'
+import { formatUnits } from 'viem'
+
+import { useStakingDashboard } from '../../_context/stakingDashboardContext'
+import { useEstimateWithdrawFees } from '../../_hooks/useEstimateWithdraw'
+
+import { RetryUnlock } from './retryUnlock'
+
+type Props = {
+  onClose: VoidFunction
+}
+
+export const ReviewUnlock = function ({ onClose }: Props) {
+  const { unlockingDashboardOperation } = useStakingDashboard()
+  const token = useHemiToken()
+
+  // unlockingDashboardOperation is defined because this component is only rendered in that case
+  const { stakingPosition, status } = unlockingDashboardOperation!
+
+  const unlockStatus = status ?? UnlockingDashboardStatus.UNLOCK_TX_CONFIRMED
+
+  const t = useTranslations('staking-dashboard.drawer')
+  const hemi = useHemi()
+
+  const { fees: withdrawGasFees, isError: isWithdrawGasFeesError } =
+    useEstimateWithdrawFees({
+      token,
+      tokenId: BigInt(stakingPosition!.tokenId),
+    })
+
+  const getStepFees = ({
+    fee,
+    isError,
+    show,
+  }: {
+    fee: bigint
+    show: boolean
+    isError: boolean
+  }): StepPropsWithoutPosition['fees'] =>
+    show
+      ? {
+          amount: formatUnits(fee, hemi.nativeCurrency.decimals),
+          isError,
+          token: getNativeToken(hemi.id),
+        }
+      : undefined
+
+  const addUnlockingStep = function (): StepPropsWithoutPosition {
+    const statusMap: Record<UnlockingDashboardStatus, ProgressStatusType> = {
+      [UnlockingDashboardStatus.UNLOCK_TX_PENDING]: ProgressStatus.PROGRESS,
+      [UnlockingDashboardStatus.UNLOCK_TX_FAILED]: ProgressStatus.FAILED,
+      [UnlockingDashboardStatus.UNLOCK_TX_CONFIRMED]: ProgressStatus.COMPLETED,
+    }
+    const showFees = [
+      UnlockingDashboardStatus.UNLOCK_TX_PENDING,
+      UnlockingDashboardStatus.UNLOCK_TX_FAILED,
+    ].includes(unlockStatus)
+
+    return {
+      description: (
+        <ChainLabel
+          active={unlockStatus === UnlockingDashboardStatus.UNLOCK_TX_PENDING}
+          chainId={hemi.id}
+          label={t('unlock-token', { symbol: token.symbol })}
+        />
+      ),
+      explorerChainId: token.chainId,
+      fees: getStepFees({
+        fee: withdrawGasFees,
+        isError: isWithdrawGasFeesError,
+        show: showFees,
+      }),
+      status: statusMap[unlockStatus] ?? ProgressStatus.NOT_READY,
+      txHash: unlockingDashboardOperation?.transactionHash,
+    }
+  }
+
+  const getSteps = () => [addUnlockingStep()]
+
+  const getCallToAction = (callStatus: UnlockingDashboardStatus) =>
+    [UnlockingDashboardStatus.UNLOCK_TX_FAILED].includes(callStatus) ? (
+      <RetryUnlock />
+    ) : null
+
+  return (
+    <Operation
+      amount={stakingPosition!.amount.toString()}
+      callToAction={getCallToAction(unlockStatus)}
+      heading={t('unlock.heading')}
+      onClose={onClose}
+      steps={getSteps()}
+      subheading={t('unlock.subheading')}
+      token={token}
+    />
+  )
+}
---
+++ portal/app/[locale]/staking-dashboard/_hooks/useUnlock.ts
@@ -0,0 +1,143 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { EventEmitter } from 'events'
+import { useNativeTokenBalance, useTokenBalance } from 'hooks/useBalance'
+import { useHemiWalletClient } from 'hooks/useHemiClient'
+import { useUpdateNativeBalanceAfterReceipt } from 'hooks/useInvalidateNativeBalanceAfterReceipt'
+import { useUmami } from 'hooks/useUmami'
+import {
+  StakingDashboardToken,
+  StakingPosition,
+  StakingPositionStatus,
+  UnlockingDashboardOperation,
+  UnlockingDashboardStatus,
+} from 'types/stakingDashboard'
+import { WithdrawEvents } from 've-hemi-actions'
+import { withdraw } from 've-hemi-actions/actions'
+import { useAccount } from 'wagmi'
+
+import { useDrawerStakingQueryString } from './useDrawerStakingQueryString'
+import { getStakingPositionsQueryKey } from './useStakingPositions'
+
+type UseUnlock = {
+  amount: bigint
+  on?: (emitter: EventEmitter<WithdrawEvents>) => void
+  token: StakingDashboardToken
+  tokenId: string
+  updateUnlockingDashboardOperation: (
+    payload?: UnlockingDashboardOperation,
+  ) => void
+}
+
+export const useUnlock = function ({
+  amount,
+  on,
+  token,
+  tokenId,
+  updateUnlockingDashboardOperation,
+}: UseUnlock) {
+  const { setDrawerQueryString } = useDrawerStakingQueryString()
+  const { track } = useUmami()
+  const { address } = useAccount()
+  const queryClient = useQueryClient()
+  const { queryKey: hemiBalanceQueryKey } = useTokenBalance(
+    token.chainId,
+    token.address,
+  )
+
+  const stakingPositionQueryKey = getStakingPositionsQueryKey({
+    address,
+    chainId: token.chainId,
+  })
+
+  const { queryKey: nativeTokenBalanceQueryKey } = useNativeTokenBalance(
+    token.chainId,
+  )
+
+  const updateNativeBalanceAfterFees = useUpdateNativeBalanceAfterReceipt(
+    token.chainId,
+  )
+
+  const { hemiWalletClient } = useHemiWalletClient()
+
+  return useMutation({
+    mutationFn: function runWithdraw() {
+      if (!address) {
+        throw new Error('No account connected')
+      }
+      const { emitter, promise } = withdraw({
+        account: address,
+        tokenId: BigInt(tokenId),
+        walletClient: hemiWalletClient!,
+      })
+
+      emitter.on('user-signed-withdraw', function (transactionHash) {
+        updateUnlockingDashboardOperation({
+          stakingPosition: { amount, tokenId },
+          status: UnlockingDashboardStatus.UNLOCK_TX_PENDING,
+          transactionHash,
+        })
+        setDrawerQueryString('unlocking')
+
+        track?.('staking dashboard - signed withdraw')
+      })
+      emitter.on('user-signing-withdraw-error', function () {
+        updateUnlockingDashboardOperation({
+          status: UnlockingDashboardStatus.UNLOCK_TX_FAILED,
+        })
+
+        track?.('staking dashboard - signing withdraw error')
+      })
+      emitter.on('withdraw-transaction-succeeded', function (receipt) {
+        updateUnlockingDashboardOperation({
+          status: UnlockingDashboardStatus.UNLOCK_TX_CONFIRMED,
+        })
+
+        queryClient.setQueryData(
+          stakingPositionQueryKey,
+          (old: StakingPosition[] | undefined = []) =>
+            old.map(position =>
+              position.tokenId === tokenId
+                ? { ...position, status: StakingPositionStatus.WITHDRAWN }
+                : position,
+            ),
+        )
+
+        // fees
+        updateNativeBalanceAfterFees(receipt)
+        // HEMI balance
+        queryClient.setQueryData(
+          hemiBalanceQueryKey,
+          (old: bigint) => old + amount,
+        )
+
+        track?.('staking dashboard - withdraw success')
+      })
+      emitter.on('withdraw-transaction-reverted', function (receipt) {
+        updateUnlockingDashboardOperation({
+          status: UnlockingDashboardStatus.UNLOCK_TX_FAILED,
+        })
+
+        // Although the transaction was reverted, the gas was paid.
+        updateNativeBalanceAfterFees(receipt)
+
+        track?.('staking dashboard - withdraw transaction reverted')
+      })
+
+      on?.(emitter)
+
+      return promise
+    },
+    onSettled() {
+      // Do not return the promises here. Doing so will delay the resolution of
+      // the mutation, which will cause the UI to be out of sync until balances are re-validated.
+      // Query invalidation here must work as fire and forget, as, after all, it runs in the background!
+      queryClient.invalidateQueries({
+        queryKey: hemiBalanceQueryKey,
+      })
+
+      queryClient.invalidateQueries({
+        queryKey: nativeTokenBalanceQueryKey,
+      })
+    },
+  })
+}

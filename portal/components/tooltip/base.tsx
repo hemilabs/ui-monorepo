1b679b4d2f76e9ebead62941a74dbd3b4e255b16
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-10-01 17:12:18 -0300

    Bump rc-tooltip version to latest

--- portal/components/tooltip/base.tsx
+++ portal/components/tooltip/base.tsx
@@ -21,24 +21,23 @@ export const BaseTooltip = ({
     align={{
       overflow: { adjustX: true, adjustY: true },
     }}
+    classNames={{
+      root: `shadow-soft max-w-[360px] bg-neutral-800
+        text-white [&_.rc-tooltip-inner]:min-h-0 [&_.rc-tooltip-inner]:border
+        [&_.rc-tooltip-inner]:border-solid [&_.rc-tooltip-inner]:border-black/85
+        [&_.rc-tooltip-inner]:bg-neutral-800 [&_.rc-tooltip-inner]:p-0`,
+    }}
     destroyTooltipOnHide
     id={id}
     overlay={overlay}
-    overlayClassName="shadow-soft max-w-[360px] bg-neutral-800
-        text-white [&_.rc-tooltip-inner]:min-h-0 [&_.rc-tooltip-inner]:border
-        [&_.rc-tooltip-inner]:border-solid [&_.rc-tooltip-inner]:border-black/85
-        [&_.rc-tooltip-inner]:bg-neutral-800 [&_.rc-tooltip-inner]:p-0"
-    // these can't be set by overlayClassName
-    overlayInnerStyle={{
-      borderRadius,
-    }}
-    overlayStyle={{
-      borderRadius,
-      opacity: 1,
-      padding: 0,
-    }}
     placement={placement}
     showArrow={false}
+    styles={{
+      body: {
+        borderRadius,
+      },
+      root: { borderRadius, opacity: 1, padding: 0 },
+    }}
     trigger={trigger}
   >
     {children}

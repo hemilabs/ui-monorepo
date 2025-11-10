ea31a3d6754556fffdedbc4254253db8f1afa390
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-11-10 10:39:43 -0300

    Rename variables for better readability

--- portal/hooks/useHemiBtcToken.ts
+++ portal/hooks/useHemiBtcToken.ts
@@ -3,28 +3,28 @@ import { useMemo } from 'react'
 import { EvmToken } from 'types/token'
 import { getTokenByAddress } from 'utils/token'
 import { type Address, type Chain } from 'viem'
-import { hemi as hemiMainnet, hemiSepolia } from 'viem/chains'
+import { hemi, hemiSepolia } from 'viem/chains'
 
 const HEMI_BTC_ADDRESSES: Record<Chain['id'], Address> = {
-  [hemiMainnet.id]: '0xAA40c0c7644e0b2B224509571e10ad20d9C4ef28',
+  [hemi.id]: '0xAA40c0c7644e0b2B224509571e10ad20d9C4ef28',
   [hemiSepolia.id]: '0x36Ab5Dba83d5d470F670BC4c06d7Da685d9afAe7',
 }
 
 export const useHemiBtcToken = function () {
-  const hemi = useHemi()
+  const hemiChain = useHemi()
 
   return useMemo(
     function () {
       const address =
-        HEMI_BTC_ADDRESSES[hemi.id as keyof typeof HEMI_BTC_ADDRESSES]
+        HEMI_BTC_ADDRESSES[hemiChain.id as keyof typeof HEMI_BTC_ADDRESSES]
       if (!address) {
         throw new Error(
-          `HemiBTC token address not found for chain ID: ${hemi.id}`,
+          `HemiBTC token address not found for chain ID: ${hemiChain.id}`,
         )
       }
       // hemiBTC is always an EvmToken
-      return getTokenByAddress(address, hemi.id) as EvmToken
+      return getTokenByAddress(address, hemiChain.id) as EvmToken
     },
-    [hemi.id],
+    [hemiChain.id],
   )
 }

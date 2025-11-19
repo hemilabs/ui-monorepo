deba1e59b3aaf54f86d546a4712de28f4c3132da
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-11-19 10:46:49 -0300

    Enable/Disable actions given user state

--- portal/app/[locale]/bitcoin-yield/_components/actions.tsx
+++ portal/app/[locale]/bitcoin-yield/_components/actions.tsx
@@ -1,32 +1,58 @@
 import { Button } from 'components/button'
+import { Spinner } from 'components/spinner'
+import { useTokenBalance } from 'hooks/useBalance'
 import { useTranslations } from 'next-intl'
 
 import { useOperationDrawer } from '../_hooks/useOperationDrawer'
+import { usePoolAsset } from '../_hooks/usePoolAsset'
+import { useUserPoolBalance } from '../_hooks/useUserPoolBalance'
 
 export const Actions = function () {
   const t = useTranslations()
   const [, setOperationDrawer] = useOperationDrawer()
+  const token = usePoolAsset().data
+
+  const {
+    balance,
+    // I need to use this because useTokenBalance is defaulting to zero
+    // TODO https://github.com/hemilabs/ui-monorepo/issues/1648
+    isLoading: isTokenBalanceLoading,
+  } = useTokenBalance(token.chainId, token.address)
+  const { data: poolBalance, isError: isPoolBalanceError } =
+    useUserPoolBalance()
 
   const commonProps = {
     size: 'xSmall',
     type: 'button',
   } as const
 
+  const poolBalanceLoading = poolBalance === undefined && !isPoolBalanceError
+
   return (
-    <div className="flex items-center gap-x-3">
+    <div className="flex items-center gap-x-3 [&>button]:min-w-16">
       <Button
         {...commonProps}
+        disabled={balance === undefined || balance === BigInt(0)}
         onClick={() => setOperationDrawer('deposit')}
         variant="primary"
       >
-        {t('common.deposit')}
+        {isTokenBalanceLoading ? (
+          <Spinner size="xSmall" />
+        ) : (
+          t('common.deposit')
+        )}
       </Button>
       <Button
         {...commonProps}
+        disabled={poolBalance === undefined || poolBalance === BigInt(0)}
         onClick={() => setOperationDrawer('withdraw')}
         variant="secondary"
       >
-        {t('common.withdraw')}
+        {poolBalanceLoading ? (
+          <Spinner color="#FF6C15" size="xSmall" />
+        ) : (
+          t('common.withdraw')
+        )}
       </Button>
       <Button {...commonProps} variant="secondary">
         {t('bitcoin-yield.table.claim-rewards')}

d8091494597f0aefd094dc5a48dd9f631c1bbba6
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-12-01 20:26:56 -0300

    Add hover effect on chevron button

--- portal/app/[locale]/bitcoin-yield/_components/actions.tsx
+++ portal/app/[locale]/bitcoin-yield/_components/actions.tsx
@@ -64,24 +64,26 @@ export const Actions = function ({ row }: Props) {
           <Claim />
         </div>
       )}
-      <ButtonIcon
-        {...commonProps}
-        disabled={loadingStrategies || !row.getCanExpand()}
-        onClick={function (e) {
-          // we need to stop propagation as otherwise the row handler will execute
-          e.stopPropagation()
-          row.toggleExpanded()
-        }}
-        variant="secondary"
-      >
-        {loadingStrategies ? (
-          <Spinner color="#FF6C15" size="xSmall" />
-        ) : row.getIsExpanded() ? (
-          <Chevron.Up />
-        ) : (
-          <Chevron.Bottom />
-        )}
-      </ButtonIcon>
+      <div className="group/chevron">
+        <ButtonIcon
+          {...commonProps}
+          disabled={loadingStrategies || !row.getCanExpand()}
+          onClick={function (e) {
+            // we need to stop propagation as otherwise the row handler will execute
+            e.stopPropagation()
+            row.toggleExpanded()
+          }}
+          variant="secondary"
+        >
+          {loadingStrategies ? (
+            <Spinner color="#FF6C15" size="xSmall" />
+          ) : row.getIsExpanded() ? (
+            <Chevron.Up className="group-hover/chevron:[&>path]:fill-neutral-950 group-hover/chevron:[&>path]:transition-colors" />
+          ) : (
+            <Chevron.Bottom className="group-hover/chevron:[&>path]:fill-neutral-950 group-hover/chevron:[&>path]:duration-200" />
+          )}
+        </ButtonIcon>
+      </div>
     </div>
   )
 }

9cfb3ddc7d3118bbf266e7a8a01fe5c969872632
Gabriel Montes <gabriel@hemi.xyz>
2025-12-01 20:14:53 -0500

    Add OKX bitcoin wallet connectors

--- portal/context/btcWalletContext.tsx
+++ portal/context/btcWalletContext.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { bitcoinTestnet, bitcoinMainnet } from 'btc-wallet/chains'
+import { okx } from 'btc-wallet/connectors/okx'
 import { unisat } from 'btc-wallet/connectors/unisat'
 import { BtcWalletProvider } from 'btc-wallet/context/btcWalletContext'
 import { type ReactNode } from 'react'
 
 const btcWalletConfig = {
   chains: [bitcoinMainnet, bitcoinTestnet],
-  connectors: [unisat],
+  connectors: [unisat, okx],
 }
 
 type Props = {

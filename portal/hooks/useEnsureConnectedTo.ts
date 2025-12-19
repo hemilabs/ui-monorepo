aa27c43a56d7d880beb46cd8b719ab4e65571ef9
Ariel Mergen <ariel@hemi.xyz>
2025-12-19 17:25:32 -0300

    Add to useEnsureConnectedTo check to btc

--- portal/hooks/useEnsureConnectedTo.ts
+++ portal/hooks/useEnsureConnectedTo.ts
@@ -1,16 +1,35 @@
-import { Chain } from 'viem'
+import { useAccount as useBtcAccount } from 'btc-wallet/hooks/useAccount'
+import { type RemoteChain } from 'types/chain'
+import { isBtcNetworkId, isEvmNetworkId } from 'utils/chain'
 import { useAccount, useSwitchChain } from 'wagmi'
 
 export const useEnsureConnectedTo = function () {
   const { switchChainAsync } = useSwitchChain()
-  const { address, chainId } = useAccount()
+  const { address: evmAddress, chainId: evmChainId } = useAccount()
+  const {
+    address: btcAddress,
+    chainId: btcChainId,
+    connector: btcConnector,
+  } = useBtcAccount()
 
-  return async function ensureConnectedTo(targetChainId: Chain['id']) {
-    if (!address) {
-      throw new Error('No account connected')
+  return async function ensureConnectedTo(targetChainId: RemoteChain['id']) {
+    if (isEvmNetworkId(targetChainId)) {
+      if (!evmAddress) throw new Error('No EVM account connected')
+      if (evmChainId !== targetChainId) {
+        await switchChainAsync({ chainId: targetChainId })
+      }
+      return
     }
-    if (targetChainId !== chainId) {
-      await switchChainAsync({ chainId: targetChainId })
+
+    if (isBtcNetworkId(targetChainId)) {
+      if (!btcAddress) throw new Error('No Bitcoin account connected')
+      if (!btcConnector) throw new Error('Bitcoin wallet not connected')
+      if (btcChainId !== targetChainId) {
+        await btcConnector.switchNetwork(targetChainId)
+      }
+      return
     }
+
+    throw new Error(`Unsupported chain type: ${targetChainId}`)
   }
 }

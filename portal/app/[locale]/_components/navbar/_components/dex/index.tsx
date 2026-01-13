ed962751bf383a60b92c7072aade17b6679cbe6c
Ariel Mergen <ariel@hemi.xyz>
2026-01-13 16:34:24 -0300

    Fix capitalized text in pt

--- portal/app/[locale]/_components/navbar/_components/dex/index.tsx
+++ portal/app/[locale]/_components/navbar/_components/dex/index.tsx
@@ -136,7 +136,7 @@ const DexImpl = function () {
       <Row>
         <IconContainer selected={isOpen}>{<DexIcon />}</IconContainer>
         <ItemText selected={isOpen} text={t('title')} />
-        <Chevron.Right className="ml-auto group-hover/nav:block max-md:hidden" />
+        <Chevron.Right className="grup ml-auto hidden group-hover/nav:block group-hover/item:flex max-md:hidden [&>path]:fill-neutral-500" />
       </Row>
 
       {isOpen &&

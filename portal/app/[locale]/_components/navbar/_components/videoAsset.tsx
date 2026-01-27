1f38b79f355981b71cf640c59990bbb289a7c445
arturdolzan <arturdolzan@gmail.com>
2026-01-27 13:02:19 -0300

    Fix social links hidden by video on testnet navbar

--- portal/app/[locale]/_components/navbar/_components/videoAsset.tsx
+++ portal/app/[locale]/_components/navbar/_components/videoAsset.tsx
@@ -1,16 +1,23 @@
+import { useNetworkType } from 'hooks/useNetworkType'
 import { useWindowSize } from 'hooks/useWindowSize'
 import { Suspense } from 'react'
 import { screenBreakpoints } from 'styles'
 
 const VideoAssetImpl = function () {
   const { width } = useWindowSize()
+  const [networkType] = useNetworkType()
+  const isNotTestnet = networkType !== 'testnet'
 
   if (width < screenBreakpoints.md) {
     return null
   }
 
   return (
-    <div className="w-54 h-30 -translate-y-22 absolute scale-150 overflow-x-hidden">
+    <div
+      className={`w-54 h-30 absolute scale-150 overflow-x-hidden ${
+        isNotTestnet ? '-translate-y-22' : '-translate-y-30'
+      }`}
+    >
       <div className="flex h-full w-[270px]">
         <video autoPlay className="h-auto w-full" loop muted preload="none">
           <source src="/navbarVideo.mp4" type="video/mp4" />

b124fe21f838a08b1973eba70463f2e235de1bd2
Ariel Mergen <ariel@hemi.xyz>
2026-04-02 11:56:22 -0300

    Implements useEstimateApproveErc20Fees

--- portal/hooks/useEstimateApproveErc20Fees.ts
+++ portal/hooks/useEstimateApproveErc20Fees.ts
@@ -1,33 +1,27 @@
+import { useEstimateApproveErc20Fees as useEstimateApproveErc20FeesFromLib } from '@hemilabs/react-hooks/useEstimateApproveErc20Fees'
 import { useEstimateFees } from 'hooks/useEstimateFees'
-import { EvmToken } from 'types/token'
-import { Address, encodeFunctionData, erc20Abi } from 'viem'
+import { encodeFunctionData, erc20Abi } from 'viem'
 import { useEstimateGas } from 'wagmi'
 
-export const useEstimateApproveErc20Fees = function ({
-  amount,
-  enabled = true,
-  spender,
-  token,
-}: {
-  amount: bigint
-  enabled?: boolean
-  spender: Address
-  token: EvmToken
-}) {
+type LibParams = Parameters<typeof useEstimateApproveErc20FeesFromLib>[0]
+
+export const useEstimateApproveErc20Fees = function (params: LibParams) {
+  const { amount, enabled, spender, token } = params
+
   const { data: gasUnits, isError } = useEstimateGas({
+    chainId: token.chainId,
     data: encodeFunctionData({
       abi: erc20Abi,
       args: [spender, amount],
       functionName: 'approve',
     }),
     query: { enabled: enabled && amount > BigInt(0) },
-    to: token.address as Address,
+    to: token.address,
   })
 
   return useEstimateFees({
     chainId: token.chainId,
     gasUnits,
     isGasUnitsError: isError,
-    overEstimation: 1.5,
   })
 }

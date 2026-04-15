af19a1d132b1c1c80836161ac4ad8e9d0a478122
Ariel Mergen <ariel@hemi.xyz>
2026-04-15 17:26:04 -0300

    Fix comment

--- portal/app/[locale]/_components/navbar/navbarResponsive.tsx
+++ portal/app/[locale]/_components/navbar/navbarResponsive.tsx
@@ -9,7 +9,7 @@ import { NavbarMobile } from './navbarMobile'
 export const NavbarResponsive = function () {
   const { width } = useWindowSize()
   if (width >= screenBreakpoints.xl) {
-    // as of lg breakpoint in tailwind, it should not render anything
+    // as of xl breakpoint in tailwind, it should not render anything
     return null
   }
   return width >= screenBreakpoints.md ? <NavbarDesktop /> : <NavbarMobile />

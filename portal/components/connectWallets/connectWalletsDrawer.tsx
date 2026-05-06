0ee453d2dccfda8efad771ddb1dcff6eca79a483
arturdolzan <arturdolzan@gmail.com>
2026-05-06 19:28:53 +0200

    Track close wallet drawer in ConnectWalletsDrawer

--- portal/components/connectWallets/connectWalletsDrawer.tsx
+++ portal/components/connectWallets/connectWalletsDrawer.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { DrawerParagraph, DrawerTopSection } from 'components/drawer'
+import { useUmami } from 'hooks/useUmami'
 import { useTranslations } from 'next-intl'
+import { useCallback } from 'react'
 
 import { BtcWallet } from './wallets/btcWallet'
 import { EvmWallet } from './wallets/evmWallet'
@@ -12,13 +14,22 @@ type Props = {
 
 export const ConnectWalletsDrawer = function ({ closeDrawer }: Props) {
   const t = useTranslations()
+  const { track } = useUmami()
+
+  const onClose = useCallback(
+    function onClose() {
+      track?.('close wallet drawer')
+      closeDrawer()
+    },
+    [closeDrawer, track],
+  )
 
   return (
     <div className="drawer-content !min-h-full sm:pb-6 md:max-w-md">
       <div className="mb-3 flex flex-col gap-y-3">
         <DrawerTopSection
           heading={t('common.connect-wallets')}
-          onClose={closeDrawer}
+          onClose={onClose}
         />
         <div className="mt-5">
           <EvmWallet />

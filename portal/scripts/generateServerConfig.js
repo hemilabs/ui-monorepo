248c927bbb95b09993cd28b0be7250135cfff3e8
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-05-29 16:32:18 -0300

    Wire up NEXT_PUBLIC_VETRO_API_URL for Hemi Earn
    The Portal calls the Vetro API to power the Hemi Earn page
    (variable-stake APY and user rewards), but the env var was not
    wired up anywhere. Following portal/Environment Variables.md:
    
    - Document it in portal/README.md
    - Allow its origin in the CSP connect-src (generateServerConfig.js)
    - Declare and forward it through the deploy-portal action
    - Pass it from secrets in both Hostinger deployment jobs
    
    Sourced from a GitHub Actions secret until a public prod URL exists.

--- portal/scripts/generateServerConfig.js
+++ portal/scripts/generateServerConfig.js
@@ -77,6 +77,10 @@ if (process.env.NEXT_PUBLIC_SUBGRAPHS_API_URL) {
   fetchDomains.add(new URL(process.env.NEXT_PUBLIC_SUBGRAPHS_API_URL).origin)
 }
 
+if (process.env.NEXT_PUBLIC_VETRO_API_URL) {
+  fetchDomains.add(new URL(process.env.NEXT_PUBLIC_VETRO_API_URL).origin)
+}
+
 // If any RPC URL is customized through these env vars, the origin has to be
 // added to the allow list for the client to get the responses. Since the URLs
 // may contain a port or a path, those need to be removed as only the "origin"

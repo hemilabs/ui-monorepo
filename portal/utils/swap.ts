a6c97b7f80438725ffa1efcf03199a104679877d
Ariel Mergen <ariel@hemi.xyz>
2026-06-01 18:18:44 -0300

    Fix unused files

--- portal/utils/swap.ts
+++
@@ -1,6 +0,0 @@
-import { NetworkType } from 'hooks/useNetworkType'
-
-export const getSwapUrl = (networkType: NetworkType) =>
-  networkType === 'testnet'
-    ? 'https://swap.hemi.xyz'
-    : 'https://www.sushi.com/hemi/swap'

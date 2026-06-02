a4b6df1059ab8d2198f739f72b19a786bd704f7a
Ariel Mergen <ariel@hemi.xyz>
2026-06-02 13:36:50 -0300

    Add shadow-bs token

--- portal/app/[locale]/get-started/_components/addChain/networkConfigCard.tsx
+++ portal/app/[locale]/get-started/_components/addChain/networkConfigCard.tsx
@@ -1,11 +1,11 @@
 import { ComponentProps } from 'react'
 
 export const NetworkConfigCard = ({
-  className = '',
+  className,
   ...props
 }: ComponentProps<'div'>) => (
   <div
-    className={`rounded-lg bg-white p-4 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.05)] ${className}`}
+    className={`shadow-bs rounded-lg bg-white p-4 ${className}`}
     {...props}
   />
 )

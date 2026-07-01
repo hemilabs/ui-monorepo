6772b8b6ddb3f7dec3c287fbe6af3083e2d9ecf6
Ariel Mergen <ariel@hemi.xyz>
2026-07-01 09:01:24 -0300

    Add size control to Tabs story

--- portal/stories/tabs.stories.tsx
+++ portal/stories/tabs.stories.tsx
@@ -1,30 +1,44 @@
 import type { Meta, StoryObj } from '@storybook/nextjs'
 import { Tab, Tabs } from 'components/tabs'
+import type { ComponentProps } from 'react'
 import { useState } from 'react'
 
+type StoryProps = ComponentProps<typeof Tabs> & {
+  size: ComponentProps<typeof Tab>['size']
+}
+
 const meta = {
-  component: Tabs,
-  parameters: {
-    controls: { disable: true },
+  args: {
+    size: 'xSmall',
   },
+  argTypes: {
+    children: { control: false },
+    size: { control: 'inline-radio', options: ['xSmall', 'small'] },
+  },
+  component: Tabs,
   title: 'Components/Tabs',
-} satisfies Meta<typeof Tabs>
+} satisfies Meta<StoryProps>
 
 export default meta
 
-type Story = StoryObj<typeof Tabs>
+type Story = StoryObj<StoryProps>
 
 export const Default: Story = {
-  render: function Render() {
+  render: function Render({ size }) {
     const [filter, setFilter] = useState<'active' | 'withdrawn'>('active')
     return (
       <Tabs>
-        <Tab onClick={() => setFilter('active')} selected={filter === 'active'}>
+        <Tab
+          onClick={() => setFilter('active')}
+          selected={filter === 'active'}
+          size={size}
+        >
           Active
         </Tab>
         <Tab
           onClick={() => setFilter('withdrawn')}
           selected={filter === 'withdrawn'}
+          size={size}
         >
           Withdrawn
         </Tab>

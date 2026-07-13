50843c7a604bb50d7365a0b42310db2f1dcc6f3c
Ariel Mergen <ariel@hemi.xyz>
2026-07-13 17:04:25 -0300

    Revert changes in table stories

--- portal/stories/table.stories.tsx
+++ portal/stories/table.stories.tsx
@@ -16,7 +16,7 @@ type Row = {
 }
 
 const HemiToken = ({ className }: { className?: string }) => (
-  <svg className={className} fill="none" viewBox="0 0 20 20">
+  <svg aria-hidden="true" className={className} fill="none" viewBox="0 0 20 20">
     <rect fill="#FF6C15" height={20} rx={10} width={20} />
     <path
       d="M11.2016 3.7519C11.1372 3.74034 11.0767 3.78272 11.0653 3.84823L10.2895 8.3296H9.71049L8.93468 3.84823C8.92333 3.78272 8.86277 3.74034 8.79844 3.7519C6.04337 4.29521 3.93165 6.68425 3.75757 9.60504C3.75757 9.60889 3.75 9.7322 3.75 9.79385C3.75 9.80156 3.75 9.80926 3.75 9.81312C3.75 9.83624 3.75 9.85935 3.75 9.88247C3.75 9.89018 3.75 9.89789 3.75 9.90945C3.75 9.94027 3.75 9.96725 3.75 9.99807C3.75 13.0961 5.92227 15.674 8.80222 16.2442C8.86656 16.2558 8.92711 16.2134 8.93846 16.1479L9.71427 11.6665H10.2933L11.0653 16.1518C11.0767 16.2173 11.1372 16.2597 11.2016 16.2481C13.9566 15.7009 16.0646 13.3119 16.2424 10.3911C16.2424 10.3873 16.25 10.2639 16.25 10.2023C16.25 10.1946 16.25 10.1869 16.25 10.183C16.25 10.1599 16.25 10.1368 16.25 10.1137C16.25 10.106 16.25 10.0983 16.25 10.0867C16.25 10.0559 16.25 10.0289 16.25 9.99807C16.2538 6.90003 14.0815 4.32218 11.2016 3.7519Z"
@@ -42,8 +42,10 @@ const RewardStack = () => (
 
 const EllipsisIcon = () => (
   <svg
+    aria-hidden="true"
     className="text-neutral-500"
     fill="currentColor"
+    focusable={false}
     height={16}
     viewBox="0 0 16 16"
     width={16}
@@ -56,12 +58,15 @@ const EllipsisIcon = () => (
 
 const ActionCell = ({ timeRemaining }: { timeRemaining: string }) => (
   <div className="flex w-full items-center justify-end gap-x-2">
-    <div className="flex h-6 items-center gap-x-1 rounded-md bg-orange-600 px-2.5">
+    <button
+      className="flex h-6 items-center gap-x-1 rounded-md bg-orange-600 px-2.5"
+      type="button"
+    >
       <span className="text-xs font-semibold text-white">Unlock</span>
       <span className="flex h-4 items-center rounded-md bg-orange-100 px-1.5 text-xxs font-medium text-orange-600">
         {timeRemaining}
       </span>
-    </div>
+    </button>
     <ButtonIcon
       aria-label="More actions"
       size="xxSmall"

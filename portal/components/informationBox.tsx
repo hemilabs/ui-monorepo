c259098cb758b32117555154ed6ced45d01d2cfe
Ariel Mergen <ariel@hemi.xyz>
2026-07-21 15:58:51 -0300

    Add suggestions

--- portal/components/informationBox.tsx
+++ portal/components/informationBox.tsx
@@ -8,14 +8,14 @@ type Props = {
 }
 
 export const InformationBox = ({ actions, icon, subtitle, title }: Props) => (
-  <div className="flex h-full w-full flex-col items-center justify-center gap-y-4">
+  <div className="flex size-full flex-col items-center justify-center gap-y-4">
     <div className="flex flex-col items-center gap-y-3">
       <div className="flex size-8 items-center justify-center overflow-hidden rounded-full bg-orange-50">
         {icon}
       </div>
-      <div className="max-w-4/5 flex flex-col items-center text-center">
-        <h4 className="mb-0.5 text-sm font-semibold">{title}</h4>
-        <p className="text-sm text-neutral-500">{subtitle}</p>
+      <div className="flex flex-col items-center text-center">
+        <h4 className="mb-0.5">{title}</h4>
+        <p className="text-neutral-500">{subtitle}</p>
       </div>
     </div>
     {actions ? <div className="flex gap-x-3">{actions}</div> : null}

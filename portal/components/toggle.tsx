7d5033bcc0cadf90c6191df02624b68c4dab55ef
Ariel Mergen <ariel@hemi.xyz>
2026-09-15 12:13:50 -0300

    Simplify toggle disabled styles

--- portal/components/toggle.tsx
+++ portal/components/toggle.tsx
@@ -16,22 +16,19 @@ export const Toggle = function ({
   onCheckedChange,
 }: Props) {
   const trackStyles = checked
-    ? `bg-orange-600 ${disabled ? '' : 'hover:bg-orange-500'}`
-    : `bg-white shadow-bs ${disabled ? '' : 'hover:bg-neutral-50'}`
+    ? 'bg-orange-600 hover:bg-orange-500 has-[:disabled]:hover:bg-orange-600'
+    : 'bg-white shadow-bs hover:bg-neutral-50 has-[:disabled]:hover:bg-white'
 
   return (
     <label
-      className={`flex h-5 w-10 shrink-0 items-center rounded-full transition-colors has-[:focus-visible]:shadow-toggle-primary-focused ${trackStyles} ${
-        disabled ? 'cursor-not-allowed opacity-55' : 'cursor-pointer'
-      }`}
-      htmlFor={id}
+      className={`flex h-5 w-10 shrink-0 cursor-pointer items-center rounded-full transition-colors has-[:disabled]:cursor-not-allowed has-[:disabled]:opacity-55 has-[:focus-visible]:shadow-toggle-primary-focused ${trackStyles}`}
     >
       <input
         aria-label={ariaLabel}
         checked={checked}
-        className={`${checked ? 'translate-x-3' : 'translate-x-0'} ${
-          disabled ? 'cursor-not-allowed' : 'cursor-pointer'
-        } ml-1 block h-3 w-5 appearance-none rounded-full bg-white shadow-sm transition-transform focus:outline-none`}
+        className={`${
+          checked ? 'translate-x-3' : 'translate-x-0'
+        } ml-1 block h-3 w-5 cursor-pointer appearance-none rounded-full bg-white shadow-sm transition-transform focus:outline-none disabled:cursor-not-allowed`}
         disabled={disabled}
         id={id}
         name={id}

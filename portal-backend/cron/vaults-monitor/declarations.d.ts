efdd693c57d27c43a7fddae73587f71e35136a60
Ariel Mergen <ariel@hemi.xyz>
2026-09-24 13:35:23 -0300

    Type startinterval2 locally

--- portal-backend/cron/vaults-monitor/declarations.d.ts
+++ portal-backend/cron/vaults-monitor/declarations.d.ts
@@ -1,1 +1,9 @@
-declare module 'startinterval2'
+declare module 'startinterval2' {
+  function startInterval<A extends unknown[]>(
+    fn: (...args: A) => void,
+    delay: number,
+    ...params: A
+  ): ReturnType<typeof setInterval>
+
+  export = startInterval
+}
